7e059b6cc443aebf202d340f22e2a49a2de6eb99
Matt <mattratt@gmail.com>
2013-01-07 21:04:52 -0600

    added flask test

---
+++ demo/hello_flask.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+	return 'Hello, Flask Dude!'
+
+
+################################
+if __name__ == '__main__':
+	
+	app.run(debug=True)
+
+
+
