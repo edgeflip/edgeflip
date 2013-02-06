73f7c8ac0ea794f35baea1cca5179eea7fb683f4
Matt <mattratt@gmail.com>
2013-02-06 15:24:04 -0600

    fixed file pointer in load

--- demo/Config.py
+++ demo/Config.py
@@ -7,14 +7,21 @@ defaults = {}
 defaults['outdir'] = ''
 
 try: 
-	config = json.load('edgeflip.config')
+	config = json.load(open('edgeflip.config', 'r'))
 except AttributeError:
 	config = {}
-for k, v in defaults.items():
-	if (k not in config):
-		config[k] = v
 
 # set up logging
 logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(process)d\t%(threadName)s\t%(message)s',
-					filename=config['outdir'] + 'demo.log',
+					filename=config.get('outdir', defaults['outdir']) + 'demo.log',
 					level=logging.DEBUG)
+
+
+for k, v in config.items():
+	logging.debug("config %s: %s" % (k, str(v)))
+
+for k, v in defaults.items():
+	if (k not in config):
+		logging.debug("config default %s: %s" % (k, str(v)))
+		config[k] = v
+
