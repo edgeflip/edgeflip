9ee0741ac9afa26b44f9cbc37b12aae1a368e1ec
Kit Rodolfa <shaycrk@gmail.com>
2013-02-25 21:30:20 -0600

    make stream worker write to DB as it crawls

--- demo/stream_worker.py
+++ demo/stream_worker.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import facebook
 import database
+import datastructs
 import time
 import json
 from Config import config
@@ -15,21 +16,25 @@ logging.getLogger('pika').setLevel(logging.CRITICAL)
 
 
 def readStreamCallback(ch, method, properties, body):
-	logging.debug("got raw message %d '%s' from queue" % (readStreamCallback.messCount, body))
+	logging.debug("[worker] got raw message %d '%s' from queue" % (readStreamCallback.messCount, body))
 	readStreamCallback.messCount += 1
 	elts = json.loads(body)
 	#logging.debug("got message elts %s from queue" % str(elts))
 	userId, tok, extra = elts
-	logging.debug("received %d, %s from queue" % (userId, tok))
+	logging.debug("[worker] received %d, %s from queue" % (userId, tok))
 
 	try:
 		user = facebook.getUserFb(userId, tok)
 	except:
 		ch.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
 		return
+
+
+	logging.debug("[worker] getting friend edges from FB for %d" % userId)
+	tim = datastructs.Timer()
 	
 	conn = database.getConn()
-	database.updateUserDb(conn, user, tok, None)
+	database.updateUserDb(conn, user, tok, None) # Update the primary user in DB
 
 	skipFriends = set()
 	if (readStreamCallback.overwriteThresh != 0):
@@ -38,10 +43,42 @@ def readStreamCallback(ch, method, properties, body):
 		edgesDb = database.getFriendEdgesDb(conn, userId, readStreamCallback.requireOutgoing, newerThan=updateThresh)
 		skipFriends.update([ e.secondary.id for e in edgesDb ])
 
-	edges = facebook.getFriendEdgesFb(userId, tok, readStreamCallback.requireOutgoing, skipFriends)
-	newCount = database.updateFriendEdgesDb(user, tok, edges)
+	friends = facebook.getFriendsFb(userId, tok)
+	friendQueue = [f for f in friends if f.id not in skipFriends]
+	logging.debug("[worker] got %d friends total; updating %d of them" % ( len(friends), len(friendQueue) ))
 
-	logging.info("updated %d edges for user %d" % (newCount, userId))
+	logging.info('[worker] reading stream for user %s, %s', userId, tok)
+	sc = facebook.ReadStreamCounts(userId, tok, config['stream_days'], config['stream_days_chunk'], config['stream_threadcount'])
+	logging.debug('[worker] got %s', str(sc))
+
+	# sort all the friends by their stream rank (if any) and mutual friend count
+	friendId_streamrank = dict(enumerate(sc.getFriendRanking()))
+	logging.debug("[worker] got %d friends ranked", len(friendId_streamrank))
+	friendQueue.sort(key=lambda x: (friendId_streamrank.get(x.id, sys.maxint), -1*x.mutuals))
+
+	newCount = 0
+	curs = conn.cursor()
+	for i, friend in enumerate(friendQueue):
+		if (readStreamCallback.requireOutgoing):
+			logging.info("[worker] reading friend stream %d/%d (%s)", i, len(friendQueue), friend.id)
+			try:
+				scFriend = facebook.ReadStreamCounts(friend.id, tok, config['stream_days'], config['stream_days_chunk'], config['stream_threadcount'])
+			except Exception as ex:
+				logging.warning("[worker] error reading stream for %d: %s" % (friend.id, str(ex)))
+				continue
+			logging.debug('[worker] got %s', str(scFriend))
+			e = datastructs.EdgeSC2(user, friend, sc, scFriend)
+		else:
+			e = datastructs.EdgeSC1(user, friend, sc)
+
+		database.updateUserDb(conn, e.secondary, None, tok) # Update the secondary user in DB
+		database.updateFriendEdgeDb(curs, e) # Update the edge in DB
+		newCount += 1
+
+		logging.debug('[worker] edge %s', str(e))
+
+	conn.commit()
+	logging.debug("[worker] updated %d friend edges for %d (took: %s)" % (newCount, userId, tim.elapsedPr()))
 
 	ch.basic_ack(delivery_tag=method.delivery_tag, multiple=False)
 
