83e4d7bcf7d3b6779ec152e656feff1e70989308
Kit Rodolfa <shaycrk@gmail.com>
2013-04-04 19:56:26 -0500

    initial try at passing some session ids in url params

---
+++ demo/static/url_params.js
@@ -0,0 +1,10 @@
+function read_url_params() {
+	var urlparams = {};
+
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		urlparams[key] = value;
+		// Note -- if our param appears multiple times in the URL, this will only take the last one!
+	});
+
+	return urlparams;
+}
