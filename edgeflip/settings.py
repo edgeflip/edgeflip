18f75e0b88ad59de00d031b64dea2b67fc4dc744
Pete Fein <pete@systemadevelopment.com>
2013-04-18 16:09:59 -0500

    integrate logging in new configuration system #25 #28

--- edgeflip/settings.py
+++ edgeflip/settings.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
-import json
-import logging
-import logging.handlers
-import datetime
+import logging.config
 import os.path
-
 import pymlconf
 
 DEFAULT_CONF_DIR = os.path.join(os.path.dirname(__file__), 'conf.d')
@@ -15,13 +11,4 @@ config = pymlconf.ConfigManager(dirs=[DEFAULT_CONF_DIR], filename_as_namespace=F
 if os.path.exists(USER_CONF_DIR):
     config.load_dirs(USER_CONF_DIR, filename_as_namespace=False)
 
-# set up logging on the root logger
-def setLogger(logpath):
-    loghand = logging.handlers.TimedRotatingFileHandler(logpath, when='d', interval=1, backupCount=0, encoding=None, delay=False, utc=False)
-    logformat = logging.Formatter(fmt='%(asctime)s\t%(levelname)s\t%(process)d\t%(threadName)s\t%(message)s', datefmt=None)
-    loghand.setFormatter(logformat)
-    logger = logging.getLogger()
-    logger.handlers = []
-    logger.addHandler(loghand)
-    logger.setLevel(logging.DEBUG)
-
+logging.config.dictConfig(config.logging)
