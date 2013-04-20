237245e45d4bc7301055555b93859c0f7b65cd43
Pete Fein <pete@systemadevelopment.com>
2013-04-20 13:07:25 -0500

    integrate devel server and .wsgi with config #33

--- bin/devel_server.py
+++ bin/devel_server.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 from werkzeug.serving import run_simple
+from edgeflip.settings import config
+
+# ideally, the app to run would be determined by config? or uWSGI, hmm...
 from edgeflip.ofa_flask import app
 
-
-
-app.debug=True
+app.debug = True
 
 run_simple('localhost', 8080, app,
            use_reloader=True,
            use_debugger=True,
            passthrough_errors=False,
-           extra_files=[], # config files, etc..
+           extra_files=config.list_files(), # config files, etc..
            static_files={},
           )
--- templates/edgeflip.wsgi
+++ templates/edgeflip.wsgi
@@ -1,4 +1,7 @@
 activate_this = '$VIRTUALENV_PATH/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
+from edgeflip.settings import config
+
+# ideally, the app to run would be determined by config? or uWSGI, hmm...
 from edgeflip.ofa_flask import app as application
