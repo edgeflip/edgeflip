bb96ab20086cf07049ef2fb8aca04f29fc920c00
Pete Fein <pete@systemadevelopment.com>
2013-04-30 14:27:44 -0500

    document web.app config value #46

--- edgeflip/web/__init__.py
+++ edgeflip/web/__init__.py
@@ -1,3 +1,10 @@
+"""Package for web-related code, UI & utilities.
+
+.. envvar:: web.app
+
+    A fully-qualified module name indicating the main web app. This module must have an `app` attribute that is the WSGI app to run
+"""
+
 from ..settings import config
 import logging
 
@@ -6,8 +13,9 @@ logger = logging.getLogger(__name__)
 _app = None
 
 def getApp():
-    """return the flask app specified by configuration
+    """import the primary Flask app as specified by :envvar:`web.app`
     
+    :returns: a Flask app
     """
     global _app
     if _app is None:
