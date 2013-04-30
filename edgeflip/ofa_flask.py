acb3e702bcafd90a797f68c0d7c7ec604c228cd4
Pete Fein <pete@systemadevelopment.com>
2013-04-30 12:17:13 -0500

    move utilities from demo_flask.py to ofa_flask.py #45

--- edgeflip/ofa_flask.py
+++ edgeflip/ofa_flask.py
@@ -477,6 +477,50 @@ def rank_faces():
     return ret
     
 
+############################ UTILS #############################
+"""utility code - this should all move to scripts
+
+want big red buttons for control
+
+originally from demo_flask.py
+"""
+
+
+@app.route('/utils')
+def utils():
+    return "Combine queue and DB utils (and log reader?)"
+
+@app.route('/queue')
+def queueStatus(msg=''):
+    if (flask.request.args.get('queueName')):
+        qName = flask.request.args.get('queueName')
+    else:
+        qName = config['queue']
+    qSize = stream_queue.getQueueSize(qName)
+    uTs = time.strftime("%Y-%m-%d %H:%M:%S")
+    lName = './test_queue.txt'
+    return flask.render_template('queue.html', msg=msg, queueName=qName, queueSize=qSize, updateTs=uTs, loadName=lName)
+
+@app.route('/queue_reset')
+def queueReset():
+    qName = flask.request.args.get('queueName')
+    stream_queue.resetQueue(qName)
+    return queueStatus("Queue '%s' has been reset." % (qName))
+
+@app.route('/queue_load')
+def queueLoad():
+    qName = flask.request.args.get('queueName')
+    count = stream_queue.loadQueueFile(flask.request.args.get('queueName'), flask.request.args.get('loadPath'))
+    return queueStatus("Loaded %d entries into queue '%s'." % (count, qName))
+
+@app.route("/db_reset")
+def reset():
+    database.db.dbSetup()
+    return "database has been reset"
+
+###########################################################################
+
+
 
 if (__name__ == "__main__"):
     if ('--debug' in sys.argv):
