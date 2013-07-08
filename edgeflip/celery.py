8c4372d6b1e5866a337349ea19308d41b817347d
Mark Rogers <f4nt@f4ntasmic.com>
2013-07-08 09:48:59 -0500

    making the queues replicatable

--- edgeflip/celery.py
+++ edgeflip/celery.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
+from kombu import Queue
 
 from celery import Celery
 from edgeflip.settings import config
+
+QUEUE_ARGS = {'x-ha-policy': 'all'}
 
 celery = Celery('edgeflip.celery',
                 broker='amqp://%s:%s@%s:5672/%s' % (
@@ -22,10 +25,24 @@ celery.conf.update(
         config.dbname,
     ),
     CELERY_ALWAYS_EAGER=config.always_eager,
+    CELERY_QUEUES=(
+        Queue('px3', routing_key='px3.crawl', queue_arguments=QUEUE_ARGS),
+        Queue('px3_filter', routing_key='px3.filter', queue_arguments=QUEUE_ARGS),
+        Queue('px4', routing_key='px4.crawl', queue_arguments=QUEUE_ARGS),
+    ),
     CELERY_ROUTES={
-        'edgeflip.tasks.px3_crawl': {'queue': 'px3'},
-        'edgeflip.tasks.perform_filtering': {'queue': 'px3_filter'},
-        'edgeflip.tasks.proximity_rank_four': {'queue': 'px4'},
+        'edgeflip.tasks.px3_crawl': {
+            'queue': 'px3',
+            'routing_key': 'px3.crawl'
+        },
+        'edgeflip.tasks.perform_filtering': {
+            'queue': 'px3_filter',
+            'routing_key': 'px3.filter'
+        },
+        'edgeflip.tasks.proximity_rank_four': {
+            'queue': 'px4',
+            'routing_key': 'px4.crawl'
+        },
     }
 )
 
