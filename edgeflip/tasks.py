b46b0eb88cb9b21e7f325e6abc703838fa492643
Mark Rogers <f4nt@f4ntasmic.com>
2013-07-11 12:05:15 -0500

    retry logic on fb timeouts

--- edgeflip/tasks.py
+++ edgeflip/tasks.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from urllib2 import HTTPError
 
 from edgeflip import (
     database,
@@ -13,20 +14,23 @@ from edgeflip.settings import config
 @celery.task
 def retrieve_fb_user_info(mock_mode, fbid, token):
     ''' Retrieves FB user info and performs px3 edge ranking '''
-    if mock_mode:
-        fbmodule = mock_facebook
-    else:
-        fbmodule = facebook
-    user = fbmodule.getUserFb(fbid, token.tok)
-    edgesUnranked = fbmodule.getFriendEdgesFb(
-        fbid,
-        token.tok,
-        requireIncoming=False,
-        requireOutgoing=False
-    )
-    edgesRanked = ranking.getFriendRanking(
-        edgesUnranked, requireIncoming=False, requireOutgoing=False)
-    database.updateDb(user, token, edgesRanked,
-                      background=config.database.use_threads)
+    try:
+        if mock_mode:
+            fbmodule = mock_facebook
+        else:
+            fbmodule = facebook
+        user = fbmodule.getUserFb(fbid, token.tok)
+        edgesUnranked = fbmodule.getFriendEdgesFb(
+            fbid,
+            token.tok,
+            requireIncoming=False,
+            requireOutgoing=False
+        )
+        edgesRanked = ranking.getFriendRanking(
+            edgesUnranked, requireIncoming=False, requireOutgoing=False)
+        database.updateDb(user, token, edgesRanked,
+                        background=config.database.use_threads)
+    except HTTPError:
+        retrieve_fb_user_info.retry()
 
     return (user, edgesRanked)
