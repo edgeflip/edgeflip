e536ea2fa2ba7761cfae75a56f5bd9262ef30809
Pete Fein <pete@systemadevelopment.com>
2013-07-13 00:39:28 -0500

    create/drop scripts for dynamo

---
+++ bin/drop_dynamo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import sys
+import edgeflip.dynamo
+edgeflip.dynamo.drop_all_tables()
+print(sys.stderr, "Dropped all Dynamo tables. This make take several minutes to take effect.")
