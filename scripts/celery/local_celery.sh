72d627fcf5efb10d623313090ebfdb06e96b3d61
Mark Rogers <f4nt@f4ntasmic.com>
2013-07-15 16:33:23 -0500

    updating local_celery script

--- scripts/celery/local_celery.sh
+++ scripts/celery/local_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$VIRTUAL_ENV/bin/celery worker --app=edgeflip -l info -Q \
-    px3,px3_filter,px4
+$VIRTUAL_ENV/bin/celery worker --app=edgeflip -l info -c4 -Q \
+    px3,px3_filter,px4,celery
