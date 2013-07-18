13742c27d9f025d7c6f87ec723b6682be58b9b35
Pete Fein <pete@systemadevelopment.com>
2013-07-18 10:11:19 -0500

    minor

--- bin/create_dynamo.py
+++ bin/create_dynamo.py
@@ -2,4 +2,4 @@
 import sys
 import edgeflip.dynamo
 edgeflip.dynamo.create_all_tables()
-print(sys.stderr, "Created all Dynamo tables. This make take several minutes to take effect.")
+print>>sys.stderr, "Created all Dynamo tables. This make take several minutes to take effect."
