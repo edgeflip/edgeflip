f24290d708aaea81e1a21558b154e49e08f2b5a7
Mark Rogers <f4nt@f4ntasmic.com>
2013-07-30 17:03:52 -0500

    cleaning up static asset handling a bit

--- edgeflip/urls.py
+++ edgeflip/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,3 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('targetshare.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
