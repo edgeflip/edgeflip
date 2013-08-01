8a1159c568b46e89ec223d25a6c8acd48e7d0005
Jesse London <jesse@edgeflip.com>
2013-08-01 15:19:10 -0500

    replaced pre-Django wsgi module with Django-appropriate one

--- edgeflip/wsgi.py
+++ edgeflip/wsgi.py
@@ -15,11 +15,23 @@ framework.
 """
 import os
 
+# Activate Python virtual environment:
+activate_this = '/var/www/edgeflip/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+# Set up Django:
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "edgeflip.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edgeflip.settings")
+
+# Initialize new relic:
+from django.conf import settings
+if settings.NEWRELIC.enabled:
+    import newrelic.agent
+    newrelic.agent.initialize(settings.NEWRELIC.inifile,
+                              settings.NEWRELIC.environment)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
@@ -28,5 +40,5 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+if settings.NEWRELIC.enabled:
+    application = newrelic.agent.wsgi_application()(application)
