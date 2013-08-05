5cbd1931e2e4ef52852fee64eeee1a799bece1a5
Jesse London <jesse@edgeflip.com>
2013-08-05 12:19:21 -0500

    firmed up some new fabric utility tasks

--- fab/serve.py
+++ fab/serve.py
@@ -1,26 +1,28 @@
-""" Fabric tasks for executing various common commands """
+"""Fabric tasks for executing various common commands"""
 from fabric import api as fab
 
 from . import manage
 
 
+@fab.task(name='server', default=True)
+def start_runserver(host='0.0.0.0', port='8000'):
+    """Start the Django runserver with specified host and port"""
+    manage('runserver', args=('%s:%s' % (host, port),))
+
+
 @fab.task(name='celery')
-def start_celery(workers=4, loglevel='info', queues=None):
-    ''' Fires up celery with the specified number of workers, log level, and
-    points it at a default set of queues (or the ones specified)
-    '''
-    if queues:
-        queues = ','.join(queues)
-    else:
-        queues = 'px3,px3_filter,px4,celery'
+def start_celery(workers='4',
+                 loglevel='info',
+                 queues='px3,px3_filter,px4,celery'):
+    """Start Celery with the specified number of workers and log level
+
+    Directs Celery at the default set of queues, or those specified, e.g.:
+
+        celery:queues="px3\,px3_filter"
+
+    """
     manage('celery', args=('worker',), keyed={
-        'concurrency': workers,
+        'concurrency': int(workers),
         'loglevel': loglevel,
         'queues': queues
     })
-
-
-@fab.task(name='server')
-def start_runserver(host='127.0.0.1', port='8000'):
-    ''' Starts up the django runserver with specified host and port '''
-    manage('runserver', args=('%s:%s' % (host, port),))
