d12788d809a2750fe14836f5fab862fa4927465c
Jesse London <jesse@edgeflip.com>
2013-08-12 16:29:26 -0500

    stuck nascent admin behind authentication and authorization

--- edgeflip/urls.py
+++ edgeflip/urls.py
@@ -8,10 +8,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('targetshare.urls')),
-    url(r'^mocks/', include('targetmock.urls')),
     url(r'^admin/', include('targetadmin.urls')),
 )
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += patterns('', url(r'^simpleadmin/', include(admin.site.urls)))
+    urlpatterns += patterns('',
+        url(r'^mocks/', include('targetmock.urls')),
+        url(r'^simpleadmin/', include(admin.site.urls))
+    )
