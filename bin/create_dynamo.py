63930556b4307c298af191d0a7cc42975a111142
Jesse London <jesse@edgeflip.com>
2013-08-13 17:29:23 -0500

    moved dynamo bin/ commands into a management command "dynamo"

--- bin/create_dynamo.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import sys
-import edgeflip.dynamo
-edgeflip.dynamo.create_all_tables()
-print>>sys.stderr, "Created all Dynamo tables. This make take several minutes to take effect."
--- bin/drop_dynamo.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import sys
-import edgeflip.dynamo
-edgeflip.dynamo.drop_all_tables()
-print>>sys.stderr, "Dropped all Dynamo tables. This make take several minutes to take effect."
