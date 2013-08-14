bfc334d471c10b6da2b148d841479d2fd50c6cc3
Jesse London <jesse@edgeflip.com>
2013-08-13 21:58:11 -0500

    cleaned up base dynamo module and made its connection threaded w/o flask

--- targetshare/models/dynamo.py
+++ targetshare/models/dynamo.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-"""
-tools & classes for data stored in AWS DynamoDB
+"""tools & classes for data stored in AWS DynamoDB
 
 .. envvar:: dynamo.prefix
 
@@ -45,10 +43,10 @@ Field data lives in the incoming table; fetching outgoing edges requires a
 join to access the data. Both tables also have a local secondary index on
 `(fbid, updated)` which is used to restrict freshness to be newer than a
 given date.
+
 """
 import logging
 import threading
-import flask
 import pymlconf
 import time
 import types
@@ -62,17 +60,15 @@ from boto.dynamodb2.items import Item
 from boto.dynamodb2.fields import HashKey, RangeKey, IncludeIndex
 from boto.dynamodb2.types import NUMBER
 from django.conf import settings
-from statsd import statsd
 
 
-logger = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
-# `threading.local` for Dynamo connections created outside of flask. gross.
-_non_flask_threadlocal = threading.local()
+SCHEMAS = {} # map of table name => boto schema dict
 
 
 def _make_dynamo_aws():
-    """makes a connection to dynamo, based on configuration. For internal use.
+    """Make a connection to dynamo, based on configuration. For internal use.
 
     :rtype: dynamo connection object
 
@@ -86,61 +82,53 @@ def _make_dynamo_aws():
     return DynamoDBConnection(aws_access_key_id=access_id,
                               aws_secret_access_key=secret)
 
+
 def _make_dynamo_mock():
-    """makes a connection to mock server, based on configuration. For internal use.
+    """Make a connection to the mock dynamo server, based on configuration. For internal use.
 
     :rtype: dynamo connection object
 
     """
     # based on https://ddbmock.readthedocs.org/en/v0.4.1/pages/getting_started.html#run-as-regular-client-server
-    host='localhost'
-    port=4567
+    host = 'localhost'
+    port = settings.DYNAMO.get('port') or 4567
     endpoint = '{}:{}'.format(host, port)
     region = RegionInfo(name='mock', endpoint=endpoint)
     conn = DynamoDBConnection(aws_access_key_id="AXX", aws_secret_access_key="SEKRIT", region=region, port=port, is_secure=False)
-
     # patch the region_name so boto doesn't explode
     conn._auth_handler.region_name = "us-mock-1"
     return conn
 
-if settings.DYNAMO.engine == 'aws':
-    _make_dynamo = _make_dynamo_aws
-elif settings.DYNAMO.engine == 'mock':
-    _make_dynamo = _make_dynamo_mock
-else:
+
+def _make_dynamo():
+    """Retrive a (mock) dynamo server connection, based on configuration. For internal use."""
+    if settings.DYNAMO.engine == 'aws':
+        return _make_dynamo_aws()
+    elif settings.DYNAMO.engine == 'mock':
+        return _make_dynamo_mock()
     raise RuntimeError("Bad value {} for settings.DYNAMO.engine".format(settings.DYNAMO.engine))
 
-logger.debug("Installed engine %s", settings.DYNAMO.engine)
 
-schemas = {} # map of table name => boto schema dict
+class DynamoDBConnectionProxy(object):
+    """A lazily-connecting, thread-local proxy to a dynamo server connection."""
+    _threadlocal = threading.local()
 
-# FIXME
-def get_dynamo():
-    """return a dynamo connection for this thread.
+    @classmethod
+    def get_connection(cls):
+        try:
+            return cls._threadlocal.dynamo
+        except AttributeError:
+            cls._threadlocal.dynamo = _make_dynamo()
+            return cls._threadlocal.dynamo
 
-    All calls from the same thread return the same connection object. Do not save these or pass them around (esp. b/w threads!).
-    """
-    # largely copied from similar code in database.py
-    try:
-        dynamo = flask.g.dynamo
-    except RuntimeError as err:
-        # xxx gross, le sigh
-        if err.message != "working outside of request context":
-            raise
-        else:
-            # we are in a random thread the code spawned off from
-            # $DIETY knows where. Here, have a connection:
-            logger.debug("You made a Dynamo connection from random thread %d, and should feel bad about it.", threading.current_thread().ident)
-            try:
-                return _non_flask_threadlocal.dynamo
-            except AttributeError:
-                dynamo = _non_flask_threadlocal.dynamo = _make_dynamo()
-    except AttributeError:
-        # we are in flask-managed thread, which is nice.
-        # create a new connection & save it for reuse
-        dynamo = flask.g.dynamo= _make_dynamo()
+    def __getattr__(self, name):
+        return getattr(type(self).get_connection(), name)
 
-    return dynamo
+    def __setattr__(self, name, value):
+        return setattr(type(self).get_connection(), name, value)
+
+connection = DynamoDBConnectionProxy()
+
 
 def _table_name(name):
     """return a table name using the prefix specified in config.
@@ -149,36 +137,43 @@ def _table_name(name):
     """
     return ".".join((settings.DYNAMO.prefix, name))
 
+
 def get_table(name):
     """Return a boto table for the given name
 
     :rtype: `boto.dynamodb2.table.Table`
     """
     table = Table(_table_name(name),
-                  schema=schemas[name]['schema'],
-                  indexes=schemas[name].get('indexes', []),
-                  connection=get_dynamo())
+                  schema=SCHEMAS[name]['schema'],
+                  indexes=SCHEMAS[name].get('indexes', []),
+                  connection=connection)
     return table
+
 
 def datetime_to_epoch(dt):
     """given a datetime, return seconds since the epoch"""
     return time.mktime(dt.utctimetuple()) if dt is not None else None
 
+
 def epoch_to_datetime(epoch):
     """given seconds since the epoch, return a datetime"""
     return datetime.datetime.fromtimestamp(epoch) if epoch is not None else None
+
 
 def date_to_epoch(d):
     """given a date, return seconds since the epoch"""
     return time.mktime(d.timetuple()) if d is not None else None
 
+
 def epoch_to_date(epoch):
     """given seconds since the epoch, return a date"""
     return datetime.date.fromtimestamp(epoch) if epoch is not None else None
 
+
 def epoch_now():
     """return the current UTC time as seconds since the epoch"""
     return datetime_to_epoch(datetime.datetime.now())
+
 
 def _remove_null_values(d):
     """Modify a dict in place by deleting items having null-ish values. for internal use"""
@@ -187,50 +182,51 @@ def _remove_null_values(d):
                           types.NoneType)) and not v:
             del d[k]
 
+
 def create_table(**schema):
     """create a new table in Dynamo
 
     :arg dict schema: keyword args for `boto.dynamodb2.Table.create`
     """
     name = _table_name(schema['table_name'])
-    logger.info("Creating table %s", name)
+    LOG.info("Creating table %s", name)
     schema['table_name'] = name
-    return Table.create(connection=get_dynamo(), **schema)
+    return Table.create(connection=connection, **schema)
 
-@statsd.timer(__name__+'.create_all_tables')
+
 def create_all_tables():
     """Create all tables in Dynamo.
 
     You should only call this method once.
     """
-    dynamo = get_dynamo()
-    create_table(**schemas['users'])
-    create_table(**schemas['tokens'])
-    create_table(**schemas['edges_incoming'])
-    create_table(**schemas['edges_outgoing'])
+    create_table(**SCHEMAS['users'])
+    create_table(**SCHEMAS['tokens'])
+    create_table(**SCHEMAS['edges_incoming'])
+    create_table(**SCHEMAS['edges_outgoing'])
 
-@statsd.timer(__name__+'.drop_all_tables')
+
 def drop_all_tables():
     """Delete all tables in Dynamo"""
-    for t in schemas:
+    for t in SCHEMAS:
         try:
             get_table(t).delete()
         except StandardError as e:
-            logger.warn("Error deleting table %s: %s", t, e)
+            LOG.warn("Error deleting table %s: %s", t, e)
         else:
-            logger.debug("Deleted table %s", t)
+            LOG.debug("Deleted table %s", t)
+
 
 ##### USERS #####
 
-schemas['users'] = {
+SCHEMAS['users'] = {
     'table_name': 'users',
     'schema': [
         HashKey('fbid', data_type=NUMBER),
-        ],
+    ],
     'indexes': [],
 }
 
-@statsd.timer(__name__+'.save_user')
+
 def save_user(fbid, fname, lname, email, gender, birthday, city, state, updated=None):
     """save a user to Dynamo. If user exists, update with new, non-None attrs.
 
@@ -273,7 +269,7 @@ def save_many_users(users):
             d['birthday'] = date_to_epoch(d.get('birthday'))
             d['updated'] = updated
             _remove_null_values(d)
-            batch.put_item(data = d)
+            batch.put_item(data=d)
 
 
 def update_many_users(users):
@@ -287,11 +283,13 @@ def update_many_users(users):
     table = get_table('users')
 
     # map of fbid => data dict
-    users_data= {u['fbid']:u for u in users}
-    if not users_data: return
+    users_data = {u['fbid']: u for u in users}
+    if not users_data:
+        return
 
     for item in table.batch_get(keys=[{'fbid': k} for k in users_data]):
-        if item['fbid'] is None: continue
+        if item['fbid'] is None:
+            continue
 
         # pop the corresponding data dict
         data = users_data.pop(item['fbid'])
@@ -315,7 +313,6 @@ def update_many_users(users):
         table.put_item(data)
 
 
-@statsd.timer(__name__+'.fetch_user')
 def fetch_user(fbid):
     """Fetch a user. Returns None if user not found.
 
@@ -324,7 +321,8 @@ def fetch_user(fbid):
     """
     table = get_table('users')
     x = table.get_item(fbid=fbid)
-    if x['fbid'] is None: return None
+    if x['fbid'] is None:
+        return None
     return _make_user(x)
 
 
@@ -337,11 +335,13 @@ def fetch_many_users(fbids):
     table = get_table('users')
     # boto's BatchGetResultSet requires keys to be a list
     keys = [{'fbid': i} for i in fbids]
-    if not keys: return ()
-    logger.debug("Retrieving %d users", len(keys))
+    if not keys:
+        return ()
+    LOG.debug("Retrieving %d users", len(keys))
     results = table.batch_get(keys=keys)
     users = (_make_user(x) for x in results if x['fbid'] is not None)
     return users
+
 
 def _make_user(x):
     """make a dict from a boto Item. for internal use"""
@@ -354,7 +354,7 @@ def _make_user(x):
 
 ##### TOKENS #####
 
-schemas['tokens'] = {
+SCHEMAS['tokens'] = {
     'table_name': 'tokens',
     'schema': [
         HashKey('fbid', data_type=NUMBER),
@@ -362,7 +362,7 @@ schemas['tokens'] = {
     ]
 }
 
-@statsd.timer(__name__+'.save_token')
+
 def save_token(fbid, appid, token, expires, updated=None):
     """save a token to dynamo, overwriting existing.
 
@@ -374,17 +374,16 @@ def save_token(fbid, appid, token, expires, updated=None):
                   replaced with current timestamp.
     """
     table = get_table('tokens')
-    x = Item(table, data = dict(
-        fbid = fbid,
-        appid = appid,
-        token = token,
-        expires = datetime_to_epoch(expires),
-        updated = epoch_now()
-        ))
-
+    x = Item(table, data=dict(
+        fbid=fbid,
+        appid=appid,
+        token=token,
+        expires=datetime_to_epoch(expires),
+        updated=epoch_now()
+    ))
     return x.save(overwrite=True)
 
-@statsd.timer(__name__+'.fetch_token')
+
 def fetch_token(fbid, appid):
     """retrieve a token from facebook
 
@@ -394,7 +393,8 @@ def fetch_token(fbid, appid):
     """
     table = get_table('tokens')
     x = table.get_item(fbid=fbid, appid=appid)
-    if x['fbid'] is None:  return None
+    if x['fbid'] is None:
+        return None
     return _make_token(x)
 
 
@@ -407,8 +407,9 @@ def fetch_many_tokens(ids):
     table = get_table('tokens')
     # boto's BatchGetResultSet requires a list for keys
     keys = [{'fbid': i, 'appid': a} for i, a in ids]
-    if not keys: return ()
-    logger.debug('Retrieving %d tokens', len(keys))
+    if not keys:
+        return ()
+    LOG.debug('Retrieving %d tokens', len(keys))
     results = table.batch_get(keys=keys)
     tokens = (_make_token(x) for x in results if x['fbid'] is not None)
     return tokens
@@ -421,14 +422,15 @@ def _make_token(x):
     t['updated'] = epoch_to_datetime(t['updated'])
     return t
 
+
 ##### EDGES #####
 
-schemas['edges_outgoing'] = {
+SCHEMAS['edges_outgoing'] = {
     'table_name': 'edges_outgoing',
     'schema': [
         HashKey('fbid_source', data_type=NUMBER),
         RangeKey('fbid_target', data_type=NUMBER)
-        ],
+    ],
     'indexes': [
         IncludeIndex('updated',
                      parts=[HashKey('fbid_source', data_type=NUMBER),
@@ -437,12 +439,12 @@ schemas['edges_outgoing'] = {
     ]
 }
 
-schemas['edges_incoming'] = {
+SCHEMAS['edges_incoming'] = {
     'table_name': 'edges_incoming',
     'schema': [
         HashKey('fbid_target', data_type=NUMBER),
         RangeKey('fbid_source', data_type=NUMBER)
-        ],
+    ],
     'indexes': [
         IncludeIndex('updated',
                      parts=[HashKey('fbid_target', data_type=NUMBER),
@@ -451,7 +453,7 @@ schemas['edges_incoming'] = {
     ]
 }
 
-@statsd.timer(__name__+'.save_edge')
+
 def save_edge(fbid_source, fbid_target, **kwargs):
     """Save an edge to dynamo
 
@@ -482,8 +484,9 @@ def save_many_edges(edges):
         for e in edges:
             e['updated'] = updated
             _remove_null_values(e)
-            inc.put_item(data = e)
-            out.put_item(data = {'fbid_source': e['fbid_source'], 'fbid_target': e['fbid_target'], 'updated': updated})
+            inc.put_item(data=e)
+            out.put_item(data={'fbid_source': e['fbid_source'], 'fbid_target': e['fbid_target'], 'updated': updated})
+
 
 def save_incoming_edge(fbid_source, fbid_target, post_likes, post_comms, stat_likes, stat_comms, wall_posts, wall_comms, tags, photos_target, photos_other, mut_friends, updated):
     """save an incoming edge and its attributes to dynamo, updating.
@@ -503,16 +506,17 @@ def save_incoming_edge(fbid_source, fbid_target, post_likes, post_comms, stat_li
     x = table.get_item(fbid_source=fbid_source, fbid_target=fbid_target)
     if x['fbid_source'] is None:
         # new edge
-        logger.debug("Saving new edge %s -> %s to table %s", fbid_source, fbid_target, t)
+        LOG.debug("Saving new edge %s -> %s to table %s", fbid_source, fbid_target, t)
         table.put_item(data)
     else:
         # update existing edge. Don't even touch keys (which are
         # unchanged) because AWS/boto freak out
-        logger.debug("Updating edge %s -> %s in table %s", fbid_source, fbid_target, t)
+        LOG.debug("Updating edge %s -> %s in table %s", fbid_source, fbid_target, t)
         for k, v in data.iteritems():
             if k not in ('fbid_source', 'fbid_target'):
                 x[k] = v
         x.partial_save()
+
 
 def save_outgoing_edge(fbid_source, fbid_target, updated):
     """save an outgoing edge to dynamo, overwrites
@@ -527,11 +531,10 @@ def save_outgoing_edge(fbid_source, fbid_target, updated):
 
     t = 'edges_outgoing'
     table = get_table(t)
-    logger.debug("Saving edge %s -> %s to table %s", fbid_source, fbid_target, t)
+    LOG.debug("Saving edge %s -> %s to table %s", fbid_source, fbid_target, t)
     table.put_item(data, overwrite=True)
 
 
-@statsd.timer(__name__+'.fetch_edge')
 def fetch_edge(fbid_source, fbid_target):
     """retrieve an edge from facebook
 
@@ -554,11 +557,13 @@ def fetch_many_edges(ids):
     table = get_table('edges_incoming')
     # Boto's BatchGetResultSet requires a list for keys
     keys = [{'fbid_source': s, 'fbid_target': t} for s, t in ids]
-    if not keys: return ()
-    logger.debug('Retrieving %d edges', len(keys))
+    if not keys:
+        return ()
+    LOG.debug('Retrieving %d edges', len(keys))
     results = table.batch_get(keys=keys)
     edges = (_make_edge(x) for x in results if x['fbid_source'] is not None)
     return edges
+
 
 def fetch_all_incoming_edges():
     """Retrieve all incoming edges from dynamo
@@ -582,12 +587,12 @@ def fetch_incoming_edges(fbid, newer_than=None):
     """
     table = get_table('edges_incoming')
     if newer_than is None:
-        results = table.query(fbid_target__eq = fbid)
+        results = table.query(fbid_target__eq=fbid)
         return imap(_make_edge, results)
     else:
-        keys = table.query(index = 'updated',
-                           fbid_target__eq = fbid,
-                           updated__gt = datetime_to_epoch(newer_than))
+        keys = table.query(index='updated',
+                           fbid_target__eq=fbid,
+                           updated__gt=datetime_to_epoch(newer_than))
         return fetch_many_edges((k['fbid_source'], k['fbid_target']) for k in keys)
 
 
@@ -603,11 +608,11 @@ def fetch_outgoing_edges(fbid, newer_than=None):
     """
     table = get_table('edges_outgoing')
     if newer_than is None:
-        keys = table.query(fbid_source__eq = fbid)
+        keys = table.query(fbid_source__eq=fbid)
     else:
-        keys = table.query(index = 'updated',
-                           fbid_source__eq = fbid,
-                           updated__gt = datetime_to_epoch(newer_than))
+        keys = table.query(index='updated',
+                           fbid_source__eq=fbid,
+                           updated__gt=datetime_to_epoch(newer_than))
 
     return fetch_many_edges((k['fbid_source'], k['fbid_target']) for k in keys)
 
