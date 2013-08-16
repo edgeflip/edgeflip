cb56025acc48fcb09a43d9727d377a70d5222266
Japhy Bartlett <japhy@edgeflip.com>
2013-08-16 16:30:48 -0500

    rendering charts with fake data

--- dashboard/views.py
+++ dashboard/views.py
@@ -15,6 +15,7 @@ def dashboard(request):
 
 # google.visualization is looking for this as "cols", TODO: do we actually need an id?
 METRICS = [
+    {'id': 'label', 'label': 'Date/Hour', 'type': 'number'},
     {'id': 'visits', 'label': 'Visits', 'type': 'number'},
     {'id': 'clicks', 'label': 'Clicks', 'type': 'number'},
     {'id': 'auths', 'label': 'Authorizations', 'type': 'number'},
@@ -31,10 +32,19 @@ def fakedata(datetime):
     """ generate fake hourly / daily data in GOOG vis format """
 
     # 30 days worth of data for the monthly table
-    monthly = [{'c':[{'v':randint(1,100)} for i in range(len(METRICS))] } for i in range(30)]
+    monthly = [{'c':[{'v':randint((i*10),((i*10)+9))} for i in range(len(METRICS))] } for j in range(30)]
+
+    """ 
+    to get the chart to draw right, even though the data is ordered, the first element in each
+    row needs to be set in order:  TODO, make these actual ... days / hours
+    """
+    for i in range(30):
+        monthly[i]['c'].insert(0, {'v':i})
 
     # 24 hours worth of data for the daily table
-    daily = [{'c':[{'v':randint(1,100)} for i in range(len(METRICS))] } for i in range(25)]
+    daily = [{'c':[{'v':randint(1,100)} for i in range(len(METRICS))] } for j in range(25)]
+    for i in range(25):
+        daily[i]['c'].insert(0, {'v':i})
 
     return {'cols':METRICS, 'monthly':monthly, 'daily':daily}
 
