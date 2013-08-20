56994e77f1395532021dd7ce90fc4f06d23f805d
Japhy Bartlett <japhy@edgeflip.com>
2013-08-20 16:22:36 -0500

    working sketch for summary tables

---
+++ dashboard/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class CampaignSum(models.Model):
+    """ all of the data for a particular campaign in daily increments """
+
+    # ForeignKey to proper campaigns in the near future
+    campaign = models.CharField(max_length=256, blank=True)
+
+
+    data = models.CharField(max_length=500)  # and we jam our data in here as json
+
+
+    class Meta(object):
+        db_table = 'sum_campaign'
+
+
+class DaySum(models.Model):
+    """ A day's worth of data in hourly increments """
+
+    # ForeignKey to proper campaigns in the near future
+    campaign = models.CharField(max_length=256, blank=True)
+
+    day = models.DateField()
+    data = models.CharField(max_length=500)  # ghetto json field
+
+
+    class Meta(object):
+        db_table = 'sum_day'
