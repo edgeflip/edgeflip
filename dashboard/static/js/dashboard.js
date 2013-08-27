1f8df19aedfef95ecfab03dbbb6c54c668841c17
Japhy Bartlett <japhy@edgeflip.com>
2013-08-27 10:47:46 -0500

    toggle log scaling

--- dashboard/static/js/dashboard.js
+++ dashboard/static/js/dashboard.js
@@ -10,6 +10,14 @@ function init() {
 
     // load up datepicker widgets
     $( "#datepicker" ).datepicker({gotoCurrent:true, onSelect:getData});
+
+    // toggle for logarithmic scaling
+    window.logscale = false;
+    $('#logscale').button().click( function () {
+        window.logscale = window.logscale ? false:true;
+        $('#logscale').button( "option", "label", "Logarithmic Scaling: "+(window.logscale? "On":"Off"));
+        draw();
+        });
     }
 
 
@@ -22,11 +30,11 @@ function getData() {
 
 
 function onData(response) {
+    window.response = response;
 
     // reset valid dates in the jquery widget
-    window.response = response
-    $('#datepicker').datepicker( "option", "minDate", response.minday)
-    $('#datepicker').datepicker( "option", "maxDate", response.maxday)
+    $('#datepicker').datepicker( "option", "minDate", response.minday);
+    $('#datepicker').datepicker( "option", "maxDate", response.maxday);
 
 
     // update data tables
@@ -41,9 +49,16 @@ function onData(response) {
     var shorthour = new google.visualization.DateFormat({formatType: 'HH'});
     shorthour.format( dailydata, 0);
 
+    draw();
+    }
+
+
+
+function draw() {
+
     // redraw charts
     var dailyopts = {
-        title: 'Hourly Volume - '+response.dailyday, 
+        title: 'Hourly Volume - '+window.response.dailyday, 
         enableInteractivity: 'true', 
         width: 580, 
         height:175,
@@ -68,36 +83,9 @@ function onData(response) {
         legend: {position: 'right', textStyle: {fontSize:10}, alignment: 'end'},
 
         // "turn off" the gridlines, but keep unit labels on axes
-        vAxis: {gridlines: {color:'#FFF'}, logScale:true},
+        vAxis: {gridlines: {color:'#FFF'}, logScale:window.logscale},
         hAxis: {gridlines: {color:'#FFF', count:7}, format:'M/dd'},
         });
     }
 
 
-function mkCampaigns() {
-
-    // campaign chooser
-
-    /* 
-    // create the first second that lists out our campaigns for this client
-    var client = document.createElement('p');
-    client.innerHTML = 'Campaigns Currently Running for <b>{{client_name}}</b>';
-    var items = document.createElement('ul');
-    for (i=0; i < all_keys.length; i++)
-    {
-        var cur = document.createElement('li');
-        var cur_link = document.createElement('a');
-        var cur_link_href = "#aggregate_campaign_{0}";
-        cur_link_href = cur_link_href.replace('{0}', i.toString());
-        cur_link.href = cur_link_href;
-        cur_link.innerHTML = all_keys[i];
-        cur.appendChild(cur_link);
-        items.appendChild(cur);
-    }
-    document.body.appendChild(client);
-    document.body.appendChild(items);
-    document.body.appendChild(document.createElement('br'));
-    document.body.appendChild(document.createElement('br'));
-    document.body.appendChild(document.createElement('hr'));
-    */
-    }
