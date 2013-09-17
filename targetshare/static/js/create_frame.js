b9c1885e939b9a25ba111e0a675e1d08a08c7bd6
Jesse London <jesselondon@gmail.com>
2013-09-16 23:11:48 -0500

    removed unrelated comment

--- targetshare/static/js/create_frame.js
+++ targetshare/static/js/create_frame.js
@@ -7,7 +7,6 @@
 
 	var urlparams = {};
 
-	// TODO: refactor?
 	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
 		urlparams[key] = value;
 		// Note -- if our param appears multiple times in the URL, this will only take the last one!
