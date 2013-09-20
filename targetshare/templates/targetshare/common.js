898ca6590ab6b6f9036db20e6bfd32a196beffd3
Japhy Bartlett <japhy@edgeflip.com>
2013-09-19 21:52:07 -0500

    recordevent out of button, into common.js

--- targetshare/templates/targetshare/common.js
+++ targetshare/templates/targetshare/common.js
@@ -1,6 +1,6 @@
 {% comment %} NOTE: parent template is responsible for loading jQuery {% endcomment %}
 
-{% comment %} CSRF Stuff {% endcomment %}
+// Django CSRF magic
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie != '') {
@@ -31,5 +31,45 @@ $.ajaxSetup({
     }
 });
 
+var sessionid = '{{ session_id }}';
+var campaignid = {{ campaign.pk }};
+var contentid = {{ content.pk }};
+var content = '{{ fb_params.fb_app_name }}:button {{ goto|safe }}';
+var appid = {{ fb_params.fb_app_id }};
 
+function recordEvent(eventType, fbid, redirect, accessToken) {
+
+
+    var params = {
+        userid: fbid,
+        appid: appid,
+        content: content,
+        eventType: eventType,
+        sessionid: sessionid,
+        campaignid: campaignid,
+        contentid: contentid,
+        token: accessToken
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "{% url 'record-event' %}",
+        dataType: 'html',
+        data: params,
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Even if the event writing failed, still send the user on their way...
+            if (redirect) {
+                top.location = redirect;
+            }
+        },
+        success: function(data, textStatus, jqXHR) {
+            var header_efsid = jqXHR.getResponseHeader('X-EF-SessionID');
+            sessionid = header_efsid || sessionid;
+            if (redirect) {
+                top.location = redirect;
+            }
+        }
+    });
+
+}
 
