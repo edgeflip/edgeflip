a0eb67a4c854d16e51638f52efcdd50fa83b129d
Jesse London <jesselondon@gmail.com>
2013-10-03 12:10:53 -0500

    fixed test

--- targetshare/tests/test_views.py
+++ targetshare/tests/test_views.py
@@ -370,8 +370,10 @@ class TestEdgeFlipViews(EdgeFlipTestCase):
         )
         assignment = models.Assignment.objects.get()
         chosen_from_rows = re.findall(r'\d+', assignment.chosen_from_rows)
-        self.assertEqual({int(choice) for choice in chosen_from_rows},
-                         set(campaign.campaignbuttonstyles.values_list('pk', flat=True)))
+        self.assertEqual(
+            {int(choice) for choice in chosen_from_rows},
+            set(campaign.campaignbuttonstyles.values_list('button_style', flat=True))
+        )
         self.assertEqual(models.Event.objects.filter(event_type='session_start').count(), 1)
 
     def test_frame_faces_with_recs(self):
