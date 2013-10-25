11e2a9aefcb635580ecb5e3e9ad41b78fd6ba63b
Mark Rogers <f4nt@f4ntasmic.com>
2013-10-25 12:13:53 -0500

    cleaning up the decorator a bit

--- targetadmin/utils.py
+++ targetadmin/utils.py
@@ -13,6 +13,8 @@ def superuser_required(view):
     return decorators.user_passes_test(check_superuser)(view)
 
 
+# We could remove the internal decorator. I could see it being useful for
+# superuser only views, but we don't really have any of these now.
 def internal(view):
     is_superuser = superuser_required(view)
     logged_in_superuser = decorators.login_required(is_superuser, login_url='login')
@@ -22,13 +24,14 @@ def internal(view):
 def auth_client_required(view):
     def verify_client_auth_relation(request, *args, **kwargs):
         pk = kwargs.get('client_pk', kwargs.get('pk'))
-        if (not request.user.is_superuser and
-                not models.Client.objects.filter(
+        if (request.user.is_superuser or
+                models.Client.objects.filter(
                     pk=pk,
                     auth_groups__in=request.user.groups.all()
                 ).exists()):
-                return HttpResponseForbidden(
-                    'You do not have access to this area.'
-                )
-        return view(request, *args, **kwargs)
+            return view(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden(
+                'You do not have access to this area.'
+            )
     return decorators.login_required(verify_client_auth_relation, login_url='login')
