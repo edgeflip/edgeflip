aa322ff6f97ce65d459972cd210144959531cae7
Mark Rogers <f4nt@f4ntasmic.com>
2013-10-30 09:57:21 -0500

    making better use of the decorator

--- targetadmin/views/client_views.py
+++ targetadmin/views/client_views.py
@@ -1,6 +1,4 @@
-from django.contrib.auth import decorators
 from django.views.generic import ListView, DetailView
-from django.utils.decorators import method_decorator
 
 from targetadmin import forms, utils
 from targetshare.models import relational
@@ -21,19 +19,11 @@ class ClientListView(ListView):
                 auth_groups__user=self.request.user
             ).distinct()
 
-    @method_decorator(decorators.login_required(login_url='login'))
-    def dispatch(self, request, *args, **kwargs):
-        return super(ClientListView, self).dispatch(request, *args, **kwargs)
-
 
 class ClientDetailView(DetailView):
     model = relational.Client
     template_name = 'targetadmin/client_home.html'
     pk_url_kwarg = 'client_pk'
-
-    #@method_decorator(utils.auth_client_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(ClientDetailView, self).dispatch(request, *args, **kwargs)
 
 
 class ClientFormView(CRUDView):
@@ -42,6 +32,6 @@ class ClientFormView(CRUDView):
     success_url = 'client-detail'
     queryset = relational.Client.objects.all()
 
-client_list_view = ClientListView.as_view()
+client_list_view = utils.auth_client_required(ClientListView.as_view())
 client_detail_view = utils.auth_client_required(ClientDetailView.as_view())
 client_form_view = ClientFormView.as_view()
