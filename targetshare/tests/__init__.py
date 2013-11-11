5470ce9867eb4f6f691fb750f49c0841e7352ba2
Jesse London <jesse@edgeflip.com>
2013-11-11 17:01:43 -0600

    updated facebook test mock wrt facebook client refactor

--- targetshare/tests/__init__.py
+++ targetshare/tests/__init__.py
@@ -2,7 +2,6 @@ import json
 import os.path
 import random
 import urllib
-import urllib2
 
 from mock import Mock, patch
 
@@ -166,8 +165,8 @@ def crawl_mock():
             pass
 
         # Threaded results:
-        if isinstance(url, urllib2.Request):
-            # ThreadStreamReader
+        if 'from+stream' in url.lower():
+            # StreamReaderThread
             data = [
                 {'name': 'tags', 'fql_result_set': [{'tagged_ids': list(xrandrange(0, 25, 1))}
                                                     for _ in xrandrange(0, 25)]},
@@ -202,6 +201,9 @@ def crawl_mock():
                  'fql_result_set': [mock_client.fakeUserInfo(fbid, friend=True, numFriends=friend_count)
                                     for fbid in fake_fbids]},
             ]}
+
+        else:
+            raise RuntimeError("No mock for URL: {}".format(url))
 
     # mock to be patched on urllib2.urlopen:
     return Mock(side_effect=lambda url, timeout=None:
