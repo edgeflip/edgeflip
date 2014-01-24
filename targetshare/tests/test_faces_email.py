270200bed8722aefbf0d3fef2f69f8ad04770f43
Mark Rogers <f4nt@f4ntasmic.com>
2014-01-24 12:25:38 -0600

    restructured faces_email to use multiprocessing, tests updated

--- targetshare/tests/test_faces_email.py
+++ targetshare/tests/test_faces_email.py
@@ -20,91 +20,118 @@ class TestFacesEmail(EdgeFlipTestCase):
     def setUp(self):
         super(TestFacesEmail, self).setUp()
         self.command = faces_email.Command()
-        self.command.mock = True
-        self.command.campaign = relational.Campaign.objects.get(pk=1)
-        self.command.client = self.command.campaign.client
-        self.command.content = relational.ClientContent.objects.get(pk=1)
-        self.command.visit = relational.Visit.objects.create(
+        self.campaign = relational.Campaign.objects.get(pk=1)
+        self.client = self.campaign.client
+        self.content = relational.ClientContent.objects.get(pk=1)
+        self.visit = relational.Visit.objects.create(
             session_id='%s-%s-%s' % (
                 datetime.now().strftime('%m-%d-%y_%H:%M:%S'),
                 random.randrange(0, 1000),
                 random.randrange(0, 1000),
             ),
-            app_id=self.command.client.fb_app_id,
+            app_id=self.client.fb_app_id,
             ip='127.0.0.1',
             source='faces_email',
             visitor=relational.Visitor.objects.create(),
         )
-        self.command.num_face = 3
-        self.command.filename = 'faces_email_test.csv'
-        self.command.task_list = {}
-        self.command.edge_collection = {}
-        self.command.csv_writer = Mock()
-        self.command.file_handle = Mock()
-        self.command.failed_fbids = []
-        self.command.cache = True
-        self.command.offset = 0
+        self.num_face = 3
+        self.filename = 'faces_email_test.csv'
         self.notification = relational.Notification.objects.create(
             campaign_id=1, client_content_id=1
         )
-        self.command.notification = self.notification
         self.notification_user = relational.NotificationUser.objects.create(
             notification=self.notification, fbid=1, uuid='1',
-            app_id=self.command.client.fb_app_id,
+            app_id=self.client.fb_app_id,
         )
 
     def tearDown(self):
         try:
-            os.remove(self.command.filename)
+            os.remove(self.filename)
         except OSError:
             pass
 
         super(TestFacesEmail, self).tearDown()
 
-    def test_handle(self):
+    @patch('__builtin__.open')
+    @patch('targetshare.management.commands.faces_email.multiprocessing')
+    def test_handle(self, mp_mock, open_mock):
         ''' Test to ensure the handle method behaves properly '''
+        # Setup Mocks
+        write_mock = Mock()
+        read_mocks = [Mock(), Mock()]
+        open_mock_objs = [write_mock]
+        open_mock_objs.extend(read_mocks)
+        open_mock.side_effect = open_mock_objs
+        pool_mock = Mock()
+        pool_mock.map.return_value = ['filename_1', 'filename_2']
+        mp_mock.Pool.return_value = pool_mock
+
+        # Add more UserClients
+        expires = timezone.datetime(2020, 1, 1, 12, 0, 0, tzinfo=timezone.utc)
+        for x in range(0, 3):
+            relational.UserClient.objects.create(
+                fbid=x, client=self.client
+            )
+            token = dynamo.Token(
+                fbid=x, appid=self.client.fb_app_id,
+                token=x, expires=expires
+            )
+            token.save()
+
+        # Run the command
         command = faces_email.Command()
-        methods_to_mock = [
-            '_build_csv',
-        ]
-        pre_mocks = []
-        for method in methods_to_mock:
-            pre_mocks.append(getattr(command, method))
-            setattr(command, method, Mock())
-
         command.handle(
-            1, 1, num_face=4, output='testing.csv',
-            mock=True, url=None, cache=True, offset=0, count=None
+            1, 1, num_face=4, output=self.filename,
+            mock=True, url=None, cache=True, offset=0, count=None,
+            workers=2
         )
-        for count, method in enumerate(methods_to_mock):
-            assert getattr(command, method).called
-            setattr(command, method, pre_mocks[count])
 
-        self.assertEqual(command.campaign.pk, 1)
-        self.assertEqual(command.content.pk, 1)
-        assert self.command.mock
-        self.assertEqual(command.num_face, 4)
-        self.assertEqual(command.filename, 'testing.csv')
-        # 1 we created in setUp, the other the command did
+        # Assert lots of things
+        self.assertEqual(open_mock.call_count, 3)
+        assert write_mock.write.called
+        assert write_mock.close.called
+        self.assertEqual(
+            write_mock.write.call_args_list[0][0][0],
+            'primary_fbid,email,friend_fbids,names,html_table\n'
+        )
+        for x in read_mocks:
+            assert x.read.called
         self.assertEqual(relational.Notification.objects.count(), 2)
+
+        assert pool_mock.map.called
+        self.assertEqual(
+            pool_mock.map.call_args_list[0][0][0],
+            faces_email._handle_star_threaded
+        )
+        self.assertEqual(
+            pool_mock.map.call_args_list[0][0][1][0],
+            [2L, 1L, 1L, True, 4, 'faces_email_test.csv_part0', None, True, 0, 2]
+        )
+        self.assertEqual(
+            pool_mock.map.call_args_list[0][0][1][1],
+            [2L, 1L, 1L, True, 4, 'faces_email_test.csv_part1', None, True, 2, 4]
+        )
 
     @patch('targetshare.management.commands.faces_email.ranking')
     def test_crawl_and_filter(self, ranking_mock):
         ''' Test the _crawl_and_filter method '''
-        self.command._build_csv = Mock()
+        faces_email._build_csv = Mock()
         expires = timezone.datetime(2020, 1, 1, 12, 0, 0, tzinfo=timezone.utc)
         for x in range(0, 3):
             relational.UserClient.objects.create(
-                fbid=x, client=self.command.client
+                fbid=x, client=self.client
             )
             token = dynamo.Token(
-                fbid=x, appid=self.command.client.fb_app_id,
+                fbid=x, appid=self.client.fb_app_id,
                 token=x, expires=expires
             )
             token.save()
 
         self.command.end_count = None
-        edge_data = list(self.command._crawl_and_filter())
+        edge_data = list(faces_email._crawl_and_filter(
+            self.client, self.campaign, self.content,
+            self.notification, 0, 100, 3
+        ))
         # 4, one is pre-existing from the setUp
         self.assertEqual(
             relational.NotificationUser.objects.count(),
@@ -113,18 +140,23 @@ class TestFacesEmail(EdgeFlipTestCase):
         self.assertEqual(len(edge_data), 3)
 
     @patch_facebook
-    def test_build_csv(self):
+    @patch('targetshare.management.commands.faces_email.csv')
+    def test_build_csv(self, csv_mock):
         ''' Tests the build_csv method '''
+        writer_mock = Mock()
+        csv_mock.writer.return_value = writer_mock
         user = facebook.client.get_user(1, 1)
-        self.command.edge_collection = {
+        edge_collection = {
             self.notification_user.uuid: facebook.client.get_friend_edges(user, 1)
         }
-        self.command.url = None
-        self.command._build_csv(self.command.edge_collection.iteritems())
-        assert self.command.csv_writer.writerow.called
-        assert self.command.csv_writer.writerow.call_args[0][0][4].strip().startswith('<table')
+        faces_email._build_csv(
+            edge_collection.iteritems(), 3, 'faces_email_test.csv',
+            self.campaign, self.content, None
+        )
+        assert writer_mock.writerow.called
+        assert writer_mock.writerow.call_args[0][0][4].strip().startswith('<table')
         self.assertEqual(
-            self.command.csv_writer.writerow.call_args[0][0][1],
+            writer_mock.writerow.call_args[0][0][1],
             'fake@fake.com'
         )
         self.assertEqual(
@@ -136,17 +168,23 @@ class TestFacesEmail(EdgeFlipTestCase):
             event_type='generated').exists()
 
     @patch_facebook
-    def test_build_csv_custom_url(self):
-        self.command.url = 'http://www.google.com'
+    @patch('targetshare.management.commands.faces_email.csv')
+    def test_build_csv_custom_url(self, csv_mock):
+        writer_mock = Mock()
+        csv_mock.writer.return_value = writer_mock
+        url = 'http://www.google.com'
         user = facebook.client.get_user(1, 1)
-        self.command.edge_collection = {
+        edge_collection = {
             self.notification_user.uuid: facebook.client.get_friend_edges(user, 1)
         }
-        self.command._build_csv(self.command.edge_collection.iteritems())
-        assert self.command.csv_writer.writerow.called
-        assert 'http://www.google.com?efuuid=1' in self.command.csv_writer.writerow.call_args[0][0][4]
+        faces_email._build_csv(
+            edge_collection.iteritems(), 3, 'faces_email_test.csv',
+            self.campaign, self.content, url
+        )
+        assert writer_mock.writerow.called
+        assert 'http://www.google.com?efuuid=1' in writer_mock.writerow.call_args[0][0][4]
         self.assertEqual(
-            self.command.csv_writer.writerow.call_args[0][0][1],
+            writer_mock.writerow.call_args[0][0][1],
             'fake@fake.com'
         )
         self.assertEqual(
