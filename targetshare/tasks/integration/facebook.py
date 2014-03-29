a83a9957f74dad585983a8238839e2ea46bba1e5
Jesse London <jesselondon@gmail.com>
2014-03-29 16:44:10 -0500

    made oauth retrieval task not retry on client error (response code 400); (FB won't let us "reuse" the token retrieval code anyway).

--- targetshare/tasks/integration/facebook.py
+++ targetshare/tasks/integration/facebook.py
@@ -18,13 +18,22 @@ def store_oauth_token(client_id, code, redirect_uri):
         token_data = facebook.client.get_oauth_token(client.fb_app_id, code, redirect_uri)
         token_value = token_data['access_token']
     except (KeyError, IOError, RuntimeError) as exc:
+        # IOError should cover all networking errors, but depending on the
+        # version of requests lib, it'll descend from RuntimeError
+        content = ''
+        status_code = None
         if hasattr(exc, 'response'):
-            content = getattr(exc.response, 'content', '')
-        else:
-            content = ''
+            content = getattr(exc.response, 'content', content)
+            status_code = getattr(exc.response, 'status_code', status_code)
+
         content = content and ": {}".format(content)
-        LOG.warning("Failed to retrieve OAuth token%s", content, exc_info=True)
-        store_oauth_token.retry(exc=exc)
+
+        if status_code == 400:
+            LOG.fatal("Failed to retrieve OAuth token & giving up%s", content, exc_info=True)
+            return
+        else:
+            LOG.warning("Failed to retrieve OAuth token%s", content, exc_info=True)
+            store_oauth_token.retry(exc=exc)
 
     try:
         debug_data = facebook.client.debug_token(client.fb_app_id, token_value)
