bb385a2afcc9eaecedf59f6b04aca21bb17d48f3
Mark Rogers <f4nt@f4ntasmic.com>
2014-04-16 12:04:29 -0500

    simple mgmt command for pruning celery result tables

---
+++ core/management/commands/purge_celery_taskmeta.py
@@ -0,0 +1,28 @@
+import logging
+from datetime import timedelta
+
+from djcelery.models import TaskMeta
+
+from django.conf import settings
+from django.utils import timezone
+from django.core.management.base import NoArgsCommand
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(NoArgsCommand):
+    help = "Purges completed/failed tasks from the celery result tables"
+
+    def handle_noargs(self, **options):
+        logger.info(
+            'Purging all celery tasks that have failed/succeeded '
+            'and are over an hour old'
+        )
+        expired_time = timezone.now() - timedelta(
+            seconds=settings.CELERY_TASK_RESULT_EXPIRES)
+        TaskMeta.objects.filter(
+            status__in=['SUCCESS', 'FAILURE'],
+            date_done__lte=expired_time
+        ).delete()
+        logger.info('Purge complete')
