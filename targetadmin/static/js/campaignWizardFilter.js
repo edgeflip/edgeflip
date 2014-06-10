2cf4c16b90249795104c4acd05a32670f8997792
cbaron <chris@edgeflip.com>
2014-06-10 21:55:00 +0000

    filter page is objectified

--- targetadmin/static/js/campaignWizardFilter.js
+++ targetadmin/static/js/campaignWizardFilter.js
@@ -5,7 +5,11 @@
 
         events: {
             'click [data-js="learnMoreBtn"]': 'learnMoreClicked',
-            'click [data-js="addLayerBtn"]': 'addLayerClicked'
+            'click [data-js="addLayerBtn"]': 'addLayerClicked',
+            'click [data-js="removeLayerBtn"]': 'removeLayerClicked',
+            'dblclick .draggable': 'filterDblClicked',
+            'click [data-js="previousBtn"]': 'previousClicked',
+            'click [data-js="nextBtn"]': 'nextClicked'
         },
 
         initialize: function( options ) {
@@ -19,6 +23,14 @@
             this.slurpHtml();
 
             this.insertExistingFilters();
+
+            _.each( [ 'existingFilters', 'enabledFilters' ], function( elRef ) {
+                this.templateData[ elRef ].sortable( {
+                    connectWith: ".target-well",
+                    receive: this.filterReceived
+                } ) }, this );
+
+            $( ".sortable" ).disableSelection();
 
             this.templateData.emptyFallbackHelpText.append( this.emptyFallbackHelpText );
             this.templateData.emptyFallbackHelpIcon.data( 'content', this.emptyFallbackHelpText );
@@ -34,10 +46,10 @@
         },
 
         layerTemplate: function( data ) {
-            return '<section class="span12" data-layer="' + data.layerCount + '">' +
-            '<div class="row"><div class="span11"><h6 class="layer-text-header pull-left"><span>Fallback Audience ' + (data.layerCount - 1) + '</span></h6></div>' +
-            '<div class="span1"><h6 data-layer="' + data.layerCount + '" class="btn btn-link pull-right remove-layer">' +
-            '<i class="icon-remove"></i></h6></div></div><input type="hidden" id="id_enabled-filters-' + 
+            return '<section data-js="layerContainer" class="span12" data-layer="' + data.layerCount + '">' +
+            '<div class="row"><div class="span11"><h6 class="layer-text-header pull-left"><span data-js="layerHeading">Fallback Audience ' + (data.layerCount - 1) + '</span></h6></div>' +
+            '<div class="span1"><h6 data-js="removeLayerBtn" data-layer="' + data.layerCount + '" class="btn btn-link pull-right remove-layer">' +
+            '<i class="icon-remove"></i></h6></div></div><input data-js="layerInput" type="hidden" id="id_enabled-filters-' + 
             data.layerCount + '" name="enabled-filters-' + data.layerCount + 
             '"><div data-js="enabledFilters" class="clearfix well sortable target-well filter-well" id="enabled-filters-' + 
             data.layerCount + '">' + data.previousLayerHtml  + '</div></section>';
@@ -69,113 +81,91 @@
             if( this.model.get( 'layerCount' ) === 4 ) {
                 this.templateData.addLayerBtn.prop( 'disabled', true );
             }
-        }
+        },
 
+        removeLayerClicked: function(e) {
+            var self = this;
+
+            this.model.set( 'layerCount', this.model.get('layerCount') - 1 );
+
+            this.removeLayer( $( e.target ).closest('[data-js="layerContainer"]') );
+
+            if( this.templateData.addLayerBtn.prop( 'disabled' ) ) {
+                this.templateData.addLayerBtn.prop( 'disabled', false );
+            }
+        },
+
+        removeLayer: function( el ) {
+            var self = this;
+
+            el.fadeOut( 400, function() {
+                if( el.find('[data-js="addLayerBtn"]').length ) {
+                    $( self.$el.find('[data-js="layerContainer"]')[ self.model.get('layerCount') - 1 ] )
+                        .find('h6').first().append( self.templateData.addLayerBtn );
+                }
+                _.each( el.find('.draggable'), function( filter ) {
+                    var filterId = $(filter).attr('data-filter-id');
+                    if( this.templateData.existingFilters.find( '[data-filter-id="' + filterId + '"]' ).length === 0 &&
+                        $('#enabled-filters-1').find( 'div[data-filter-id="' + filterId + '"]' ).length === 0 ) {
+
+                        this.templateData.existingFilters.append( filter );
+                    }
+                }, self );
+
+                el.empty().remove();
+                self.updateLayerNumbers();
+            } );
+        },
+
+        updateLayerNumbers: function() {
+            
+            this.$el.find('[data-js="layerHeading"]').each( function( i, el ) {
+                $(el).text( 'Fallback Audience ' + ( i + 1 ) ) } );
+            
+            this.$el.find('[data-js="layerInput"]').each( function( i, el ) {
+                $(el).attr( 'name', 'id_enabled-filters-' + ( i + 1 ) ) } );
+        },
+
+        filterReceived: function( event, ui ) {
+            var dataLink = ui.item.attr('data-link');
+            if( dataLink ) {
+                ui.sender.find('[data-link="' + dataLink + '"]').appendTo( $(event.target) );
+            }
+        },
+
+        filterDblClicked: function( e ) {
+            var clickedEl = $(e.currentTarget),
+                dataLink = clickedEl.attr('data-link'),
+                lastEnabled = this.templateData.enabledFilters.last(),
+                availableFilters = this.templateData.existingFilters;
+
+            if( clickedEl.parent().attr('data-js') === 'existingFilters' ) {
+                if( dataLink ) {
+                    clickedEl.siblings('[data-link="' + dataLink + '"]').appendTo( lastEnabled );
+                }
+                lastEnabled.append( clickedEl );
+            } else {
+                if( dataLink ) {
+                    clickedEl.siblings('[data-link="' + dataLink + '"]').appendTo( availableFilters );
+                }
+                clickedEl.appendTo( availableFilters );
+            }
+        },
+
+        previousClicked: function() {
+            window.campaignWizardIntro.cycleCarousel();
+        },
+
+        nextClicked: function() {
+
+            _.each( this.templateData.layerContainer, function( layer ) {
+                var $layer = $(layer);
+                $( $layer.find('[data-js="layerInput"]') ).val(
+                    _.map( $layer.find('[data-js="enabledFilters"]').children(), function( filter ) {
+                        return '"' + $(filter).attr('data-filter-id').split('=')[1] + '"'
+                    }, this ) );
+            }, this );
+        }
     } );
 
 } )(jQuery);
-
-/*
-$('#add-layer').click(function(event){
-        event.preventDefault();
-        var previous_layer_html = $('#enabled-filters-' + layer_count).html()
-        var hidden_elements = $('.hidden-elements')
-        layer_count = $('#add-layer').data('layer-count') + 1;
-        $('#add-layer').data('layer-count', layer_count);
-        $('#enabled-section').append(
-            '<section class="span12" data-layer="' + layer_count + '">' +
-            '<div class="row"><div class="span11"><h6 class="layer-text-header pull-left"><span>Fallback Audience ' + (layer_count - 1) + '</span></h6></div>' +
-            '<div class="span1"><h6 data-layer="' + layer_count + '" class="btn btn-link pull-right remove-layer">' +
-            '<i class="icon-remove"></i></h6></div></div><input type="hidden" id="id_enabled-filters-' + 
-            layer_count + '" name="enabled-filters-' + layer_count + 
-            '"><div class="clearfix well sortable target-well filter-well" id="enabled-filters-' + 
-            layer_count + '">' + previous_layer_html  + '</div></section>'
-        );
-
-        $('#enabled-section').children('section').last().find('h6').first().append( $('#add-layer') );
-
-        $('#enabled-filters-' + layer_count).sortable({
-            connectWith: '.target-well',
-            receive: filterReceived
-        });
-        // Aribtrary limit, but seems about right. Subject to change
-        if (layer_count >= 4) {
-            $('#add-layer').prop('disabled', true);
-        }
-    });
-
-
-$('#filtering').on( 'click', '.remove-layer', function() {
-
-        var deleted_layer_no = $(this).data('layer'),
-            add_layer_btn = $('#add-layer');
-
-        $.each( $('section[data-layer]'), function( i, layer_section ) {
-            var $layer_section = $( layer_section );
-            if( $layer_section.data('layer') == deleted_layer_no ) {
-
-                $.each( $layer_section.find('div[data-filter-id]'), function( j, layer_filter ) {
-                    var $layer_filter = $(layer_filter),
-                        layer_filter_id = $layer_filter.data('filter-id');
-                        
-                    if( ( $('#existing-filters').find( 'div[data-filter-id="' + layer_filter_id + '"]' ).length === 0 ) &&
-                        ( $('#enabled-filters-1').find( 'div[data-filter-id="' + layer_filter_id + '"]' ).length === 0 ) ) {
-
-                        $('#existing-filters').append( $layer_filter );
-                    }
-                    
-                } );
-
-                $layer_section.fadeOut( 400, function() {
-                    var found = false;
-     
-                    if( $(this).find('#add-layer').length ) { 
-                        $('#add-layer').hide().appendTo($('body'));
-                        found = true;
-                    }
-
-                    $(this).empty().remove();
-
-                    if( found ) {
-                        if( $('#enabled-section').children('section').length ) {
-                            $('#enabled-section').children('section').last().find('h6').first().append( $('#add-layer').show() );
-                        } else {
-                            $('#enabled-section').children('h6').first().append( $('#add-layer').show() );
-                        }
-                    }
-
-                    $.each( $('*[data-layer]'), function( k, el ) {
-                        var $el = $(el),
-                            el_layer_no = $el.data('layer'),
-                            new_layer_no = el_layer_no - 1;
-
-                        if( el_layer_no > deleted_layer_no ) {
-                            
-                            $el.data( 'layer', new_layer_no );
-
-                            if( $el.prop('tagName') === 'SECTION' ) {
-
-                                $el.find('h6 span').first().text( 'Fallback Audience ' + ( new_layer_no - 1 ) );
-
-                                $('#id_enabled-filters-' + el_layer_no )
-                                    .attr( { "id": 'id_enabled-filters-' + new_layer_no,
-                                             "name": 'enabled-filters-' + new_layer_no } );
-                                           
-                                $('#enabled-filters-' + el_layer_no )
-                                    .attr( { "id": 'enabled-filters-' + new_layer_no } );
-                            }
-                        }
-                    } );
-                } );
-            }
-        } );
-
-        layer_count--;
-        add_layer_btn.data( 'layer-count', layer_count );
-
-        if( add_layer_btn.prop( 'disabled' ) ) {
-            add_layer_btn.prop( 'disabled', false );
-        }
-        
-    } );
-*/
