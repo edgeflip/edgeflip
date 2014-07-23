b939df6d7e9cafcafef42e4205864d583b43b957
Jesse London <jesse@edgeflip.com>
2014-07-23 17:03:58 -0500

    made campaign wizard create ranking key and filter upon request of topics/interest feature [T272]

--- targetadmin/tests/test_campaign_views.py
+++ targetadmin/tests/test_campaign_views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core import mail
 from django.core.urlresolvers import reverse
 
@@ -435,6 +436,96 @@ class TestCampaignViews(TestAdminBase):
                 encodeDES('{}/{}'.format(camp.pk, content.pk))
             )
         )
+
+    def test_create_campaign_wizard_topics_feature(self):
+        new_client = relational.Client.objects.create(
+            name='Test Client',
+            _fb_app_name='testing',
+            _fb_app_id=1
+        )
+        self.assertFalse(new_client.filters.exists())
+        self.assertFalse(new_client.fbobjects.exists())
+        self.assertFalse(new_client.choicesets.exists())
+        self.assertFalse(new_client.buttonstyles.exists())
+        self.assertFalse(new_client.campaigns.exists())
+        self.assertFalse(relational.FilterFeature.objects.filter(
+            filter__client=new_client
+        ).exists())
+        response = self.client.post(
+            reverse('targetadmin:campaign-wizard', args=[new_client.pk]), {
+                # Campaign Details
+                'name': 'Test Campaign',
+                'error_url': 'http://www.error.com',
+                'thanks_url': 'http://www.thanks.com',
+                'content_url': 'http://www.content.com',
+                'include_empty_fallback': 1,
+                'enabled-filters-1': '"interest.eq.Cycling","interest.eq.Health"',
+                # FB Object
+                'og_title': 'Test Title',
+                'org_name': 'Test Organization',
+                'msg1_pre': 'Hey, ',
+                'msg1_post': ' How goes it?',
+                'msg2_pre': 'Hey 2, ',
+                'msg2_post': ' How goes it 2?',
+                'og_image': 'http://imgur.com/VsiPr',
+                'sharing_prompt': 'SHARE IT',
+                'sharing_button': 'Show Your Support',
+                'og_description': 'Description of FB stuff'
+            }
+        )
+        self.assertStatusCode(response, 302)
+        camp = new_client.campaigns.latest('pk')
+        content = new_client.clientcontent.latest('pk')
+        cs = camp.campaignchoicesets.get().choice_set
+        fb_attr = camp.campaignfbobjects.get().fb_object.fbobjectattribute_set.get()
+        self.assertRedirects(response, reverse(
+            'targetadmin:campaign-wizard-finish',
+            args=[new_client.pk, camp.pk, content.pk]
+        ))
+        self.assertIn('Root', cs.name)
+        self.assertIn('Root', cs.choicesetfilters.get().filter.name)
+        self.assertTrue(new_client.filters.exists())
+        self.assertTrue(new_client.fbobjects.exists())
+        self.assertTrue(new_client.choicesets.exists())
+        self.assertTrue(new_client.buttonstyles.exists())
+        self.assertTrue(new_client.campaigns.exists())
+        self.assertEqual(new_client.campaigns.count(), 2)
+        # 1 empty, 1 new
+        self.assertEqual(new_client.filters.count(), 2)
+        self.assertEqual(new_client.choicesets.count(), 2)
+        self.assertEqual(
+            relational.FilterFeature.objects.filter(filter__client=new_client).count(),
+            2
+        )
+        self.assertEqual(fb_attr.og_action, 'support')
+        self.assertEqual(fb_attr.og_type, 'cause')
+        self.assertEqual(
+            mail.outbox[0].body,
+            'Campaign PK: {} created. Please verify it and its children.'.format(camp.pk)
+        )
+        self.assertEqual(
+            camp.campaignproperties.get().client_faces_url,
+            'https://apps.facebook.com/{}/{}/'.format(
+                new_client.fb_app_name,
+                encodeDES('{}/{}'.format(camp.pk, content.pk))
+            )
+        )
+
+        filters = cs.choicesetfilters.get().filter.filterfeatures.order_by('feature')
+        self.assertEqual([ff.feature for ff in filters], ['topics[Cycling]', 'topics[Health]'])
+        self.assertEqual({ff.feature_type.code for ff in filters}, {'topics'})
+        self.assertEqual({ff.operator for ff in filters},
+                         {relational.FilterFeature.Operator.MIN})
+        self.assertEqual({ff.decode_value() for ff in filters},
+                         {settings.ADMIN_TOPICS_FILTER_THRESHOLD})
+
+        ranking_key = camp.campaignrankingkeys.get().ranking_key
+        self.assertEqual(ranking_key.name, "{} Test Campaign".format(new_client.name))
+
+        key_features = ranking_key.rankingkeyfeatures.order_by('ordinal_position')
+        self.assertEqual([key.feature for key in key_features], ['topics[Cycling]', 'topics[Health]'])
+        self.assertEqual({key.feature_type.code for key in key_features}, {'topics'})
+        self.assertEqual({key.reverse for key in key_features}, {True})
 
     def test_create_campaign_wizard_no_filtering(self):
         new_client = relational.Client.objects.create(
