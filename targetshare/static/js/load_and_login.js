27f22d97da9d9b04409e00a11a1a1658b7ade9f0
cbaron <chris@edgeflip.com>
2014-07-30 22:54:39 +0000

    first pass at jemma's redesign, except fonts, some spacing, and button tweaks

--- targetshare/static/js/load_and_login.js
+++ targetshare/static/js/load_and_login.js
@@ -178,6 +178,7 @@ function authFailure() {
 }
 
 function displayFriendDiv(data, jqXHR) {
+    $('body').css( 'background-color', 'gray' );
     $('#your-friends-here').html(data);
     $('#your-friends-here').show();
     $('#friends_div').css('display', 'table');
