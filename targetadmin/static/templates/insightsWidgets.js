fa6b98ed2eb65f5dfff0c05529eb5167c799d4ec
cbaron <chris@edgeflip.com>
2014-08-04 21:12:28 +0000

    building the d3 widget blocks

---
+++ targetadmin/static/templates/insightsWidgets.js
@@ -0,0 +1,12 @@
+define(['vendor/handlebars'], function(Handlebars) {
+
+return Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<section class=\"col-sm-10 insights-widgets\">\n    <section id=\"horizontal-bar-chart\" class=\"row horizontal-bar-chart\"></section>\n</section>\n";
+  })
+
+});
