f499c81d49f0db84b9210634c78a0671794086b5
Jesse London <jesselondon@gmail.com>
2014-08-16 01:08:05 -0500

    took ad-hoc targeting command further [T312]

--- targetshare/management/commands/target.py
+++ targetshare/management/commands/target.py
@@ -1,35 +1,92 @@
+import itertools
 import sys
+import time
 from datetime import timedelta
-from itertools import chain
 from optparse import make_option
+from Queue import Queue
 from textwrap import dedent
+from threading import Thread
 
+from celery import group
 from django.core.management.base import CommandError, BaseCommand
 from django.utils import timezone
 
+from targetshare.integration import facebook
 from targetshare.models import dynamo, relational
 from targetshare.tasks import targeting
+
+
+def _do_debug(self, queue, results):
+    while True:
+        token = queue.get()
+
+        if token is None:
+            # Term sentinel; we're done.
+            break
+
+        result = facebook.client.debug_token(token.appid, token.token)
+        data = result['data']
+        if data['is_valid'] and data['expires_at'] > time.time():
+            results.append(token)
+
+        queue.task_done()
+
+
+def debug_tokens(tokens, max_threads=100):
+    """Test a given iterable of Tokens for validity via concurrent requests to
+    the Facebook API.
+
+    Valid, unexpired Tokens are returned in a list.
+
+    """
+    debugged = []
+    queue = Queue()
+    thread_count = 0
+    iterator = iter(tokens)
+
+    if max_threads < 1:
+        raise ValueError("max_threads must be >= 1")
+
+    while thread_count < max_threads:
+        try:
+            token = next(iterator)
+        except StopIteration:
+            break
+        else:
+            queue.put(token)
+            thread = Thread(target=_do_debug, args=(queue, debugged))
+            thread.setDaemon(True)
+            thread.start()
+            thread_count += 1
+    else:
+        for token in iterator:
+            queue.put(token)
+
+    queue.join()
+
+    for _count in xrange(thread_count):
+        queue.put(None) # relieve debugging threads
+
+    return debugged
 
 
 class Command(BaseCommand):
 
     args = '[PATH]'
     help = dedent("""\
-        Target the networks of authorized users via the specified campaign
+        Target the networks of authorized users via the specified campaign and client content
 
-            python manage.py target --campaign=150 --content=150
-            python manage.py target --campaign=150 --content=150 --limit=50
-            python manage.py target ./fbids.in --campaign=150 --content=150
-            echo 2904423 | python manage.py target - --campaign=150 --content=150
+            python manage.py target 150 150
+            python manage.py target 150 150 --limit=50
+            python manage.py target 150 150 ./fbids.in
+            echo 2904423 | python manage.py target 150 150 -
 
         """)
 
     option_list = BaseCommand.option_list + (
-        make_option('--campaign', dest='campaign_id', help='Campaign to query'),
-        make_option('--content', dest='client_content_id', help='Client content to query'),
-        make_option('--limit', type=int, help='Number of users to target (if fbids not specified)'),
+        make_option('--limit', type=int, help='Number of users to target (if FBIDs not specified)'),
         make_option('--debug', action='store_true', help='Skip tokens that fail a Facebook debugging test'),
-        make_option('--random', action='store_true', help='Select tokens randomly'),
+        make_option('--random', action='store_true', help='Select tokens randomly (if FBIDs not specified)'),
     )
 
     def __init__(self):
@@ -38,14 +95,40 @@ class Command(BaseCommand):
         self.client_content = None
         self.options = None
         self.verbosity = None
-        self.task_ids = None
+        #self.subtasks = None
 
-    def handle(self, fbidfile, **options):
-        if options['fbidfile'] and (options['limit'] or options['random']):
-            raise CommandError("option --fbidfile incompatible with options --limit and --random")
+    def pout(self, msg, verbosity=1):
+        """Write the given string to stdout as allowed by the verbosity."""
+        if self.verbosity >= verbosity:
+            self.stdout.write(msg)
 
-        self.campaign = relational.Campaign.objects.get(pk=options['campaign_id'])
-        self.client_content = relational.ClientContent.objects.get(pk=options['client_content_id'])
+    def perr(self, msg, verbosity=1):
+        """Write the given string to stderr as allowed by the verbosity."""
+        if self.verbosity >= verbosity:
+            self.stderr.write(msg)
+
+    def pplain(self, msg, verbosity=1):
+        """Write the given string to stdout as allowed by the verbosity.
+
+        No "new line" character is forced onto the end of the string; it is
+        printed "plain".
+
+        A Boolean is returned indicating whether the message was allowed by the
+        verbosity setting, (which may be used to determine whether to flush
+        stdout).
+
+        """
+        if self.verbosity >= verbosity:
+            self.stdout.write(msg, ending='')
+            return True
+        return False
+
+    def handle(self, campaign_id, client_content_id, fbidfile=None, **options):
+        if fbidfile and (options['limit'] or options['random']):
+            raise CommandError("FBID input incompatible with options --limit and --random")
+
+        self.campaign = relational.Campaign.objects.get(pk=campaign_id)
+        self.client_content = relational.ClientContent.objects.get(pk=client_content_id)
 
         if self.campaign.client != self.client_content.client:
             raise CommandError("Mismatched campaign and client content")
@@ -53,9 +136,39 @@ class Command(BaseCommand):
         self.options = options
         self.verbosity = int(options['verbosity'])
 
-        self.task_ids = tuple(self.spawn_tasks(self.stream_tokens(fbidfile)))
+        token_stream = self.stream_tokens(fbidfile)
 
-        # TODO: ...
+        if options['debug']:
+            token_stream = self.debug_stream(token_stream)
+
+        tokens = itertools.islice(token_stream, options['limit'])
+
+        subtasks = group(self.spawn_subtasks(tokens))
+        subtasks.apply_async()
+
+        self.pout("Targeting networks at proximity rank four with campaign {} "
+                  "and client content {}".format(campaign_id, client_content_id))
+        try:
+            for count in itertools.count(1):
+                self.pplain("\r")
+                self.pplain("." * count)
+                written = self.pplain(" [{} tasks completed]".format(subtasks.completed_count()))
+                if written:
+                    self.stdout.flush()
+
+                if subtasks.ready():
+                    self.pout("\n")
+                    self.pout(" ...done")
+                    break
+
+                time.sleep(1)
+        except (Exception, KeyboardInterrupt):
+            self.perr("Revoking background tasks ...")
+            subtasks.revoke()
+            self.perr(" ...done")
+            raise
+
+        # TODO: results presentation
 
     def stream_tokens(self, fbidfile):
         fb_app_id = self.campaign.client.fb_app_id
@@ -63,7 +176,7 @@ class Command(BaseCommand):
         if fbidfile:
             fh = sys.stdin if fbidfile == '-' else open(fbidfile)
             # Grab fbids separated by any space (newline, space, tab):
-            fbids = chain.from_iterable(line.split() for line in fh.xreadlines())
+            fbids = itertools.chain.from_iterable(line.split() for line in fh.xreadlines())
             keys = [{'fbid': int(fbid), 'appid': fb_app_id} for fbid in fbids]
             tokens = dynamo.Token.items.batch_get(keys)
 
@@ -81,6 +194,8 @@ class Command(BaseCommand):
                 # Some of these might be no good when debugged, so get 2x
                 num_fbids = self.options['limit'] * 2
             else:
+                # It'd be nice if boto allowed you to specify an (infinite)
+                # iterator of keys (this might be None)....
                 num_fbids = self.options['limit']
             keys = [{'fbid': fbid, 'appid': fb_app_id}
                     for fbid in random_fbids[:num_fbids].iterator()]
@@ -91,10 +206,24 @@ class Command(BaseCommand):
 
         return tokens.iterable # FIXME
 
-    def spawn_tasks(self, tokens):
+    def debug_stream(self, token_stream):
+        debug_size = 500
+        if self.options['limit'] and self.options['limit'] < debug_size:
+            debug_size = self.options['limit']
+
+        head = ()
+        while True:
+            batch = itertools.islice(token_stream, debug_size)
+            full_batch = itertools.chain(head, batch)
+            for token in debug_tokens(full_batch):
+                yield token
+
+            # Force exhausted stream to raise StopIteration for us
+            head = (next(token_stream),)
+
+    def spawn_subtasks(self, tokens):
         for token in tokens:
-            # should maybe more lax about freshness?
-            # and, don't record a visit
-            task = targeting.proximity_rank_four.delay(token, #...
-                                                    )
-            yield task.id
+            # TODO: should maybe more lax about freshness?
+            # TODO: and, don't record a visit...
+            yield targeting.proximity_rank_four.s(token, # ...
+                                                  )
