2aae33fe7b1cd5a4e9a80cab6f2b530dcc1b6bb2
Tristan Crockett <tristan@edgeflip.com>
2014-08-27 17:07:06 -0500

    Targeting figured out, more fields in place

--- targetadmin/static/js/models/campaign.js
+++ targetadmin/static/js/models/campaign.js
@@ -1,3 +1,10 @@
+var nicely_formatted_list = function(list) {
+    first_part = list.slice(0, -2);
+    last_two = list.slice(-2);
+    full_string = [first_part.join(", "), last_two.join(", or ")].filter(function(e) { return e; }).join(", ");
+    return full_string;
+};
+
 define( [ 'vendor/backbone' ], function( Backbone ) {
     return Backbone.Model.extend( {
         //attributes
@@ -7,37 +14,108 @@ define( [ 'vendor/backbone' ], function( Backbone ) {
             create_dt: undefined,
             description: undefined,
         },
-        /* transforms instantiated attributes
-           into something more usable */
+        format: function(type_code, attribute_list) {
+            if( type_code === "age" ) {
+                if(attribute_list.length == 1) {
+                    var base_message = attribute_list[0].value + " years old or "
+                    var suffix = attribute_list[0].operator === "max" ? "younger" : "older";
+                    var full_message = base_message + suffix;
+                    return full_message;
+                } else {
+                    var winning_min = 0;
+                    var winning_max = 999;
+                    for(var index in attribute_list) {
+                        if( attribute_list[index].operator === "max" ) {
+                            if( attribute_list[index].value < winning_max ) {
+                                winning_max = attribute_list[index].value
+                            }
+                        }
+                        if( attribute_list[index].operator === "min" ) {
+                            if( attribute_list[index].value > winning_min ) {
+                                winning_min = attribute_list[index].value
+                            }
+                        }
+                    }
+                    return "Between " + winning_min + " and " + winning_max + " years old"
+                }
+            } else if( type_code === "state" || type_code === "city" || type_code === "full_location") {
+                var base_message = "Lives in ";
+                var locales = attribute_list.map(function(attribute) {
+                    var locale_string;
+                    if( attribute.operator === "in" ) {
+                        locale_string = nicely_formatted_list(attribute.value.split("||"));
+                    }
+                    else {
+                        locale_string = attribute.value;
+                    }
+                    return base_message + locale_string;
+                });
+                return nicely_formatted_list(locales);
+            } else if( type_code === "topics" ) {
+                var topics = attribute_list.map(function(attribute) {
+                    return attribute.value;
+                });
+                return "Interested in" + nicely_formatted_list(topics);
+            }
+        },
         parse: function(attrs,options) {
             if( attrs.name && attrs.name.endsWith(" 1") ) {
                 attrs.name = attrs.name.substr(0, attrs.name.length-2);
             }
             attrs.create_dt = new Date(attrs.create_dt).toDateString();
             attrs.isPublished = ( attrs.campaignproperties__status === 'published' );
-            attrs.name = attrs.root_campaign;
 
-            attrs.thanks_url = attrs.campaign_properties.client_thanks_url;
-            attrs.error_url = attrs.campaign_properties.client_error_url;
+            if( attrs.root_campaign ) {
+                attrs.name = attrs.root_campaign;
+            }
 
-            attrs.og_type = attrs.fb_obj_attributes.og_type
-            attrs.suggested_message_1 = attrs.fb_obj_attributes.msg1_pre + " @name " + attrs.fb_obj_attributes.msg1_post;
-            attrs.suggested_message_2 = attrs.fb_obj_attributes.msg2_pre + " @name " + attrs.fb_obj_attributes.msg2_post;
+            if( attrs.campaign_properties ) {
+                attrs.thanks_url = attrs.campaign_properties.client_thanks_url;
+                attrs.error_url = attrs.campaign_properties.client_error_url;
+            }
+
+            if( attrs.fb_obj_attributes ) {
+                attrs.og_type = attrs.fb_obj_attributes.og_type;
+                attrs.headline = attrs.fb_obj_attributes.sharing_prompt;
+                attrs.subheader = attrs.fb_obj_attributes.sharing_sub_header;
+                attrs.suggested_message_1 = attrs.fb_obj_attributes.msg1_pre + " @name " + attrs.fb_obj_attributes.msg1_post;
+                attrs.suggested_message_2 = attrs.fb_obj_attributes.msg2_pre + " @name " + attrs.fb_obj_attributes.msg2_post;
+                attrs.og_title = attrs.fb_obj_attributes.og_title;
+                attrs.og_image = attrs.fb_obj_attributes.og_image;
+                attrs.og_text = attrs.fb_obj_attributes.og_text;
+            }
 
             if( attrs.filters ) {
+                attrs.empty_fallback = false;
                 attrs.cleaned_filters = []
-                for(var filter in attrs.filters) {
+                for(var filter_index in attrs.filters) {
                     var cleaned_filter = {}
-                    for(var attribute in filter) {
-                        cleaned_filter[attribute.feature_type__code].append(attribute)
-                    }
-                    for(var type_code in cleaned_filter) {
-                        attribute = cleaned_filter[type_code]
-                        if type_code === "age" {
+                    var filter = attrs.filters[filter_index];
+                    for(var stuff_index in filter) {
+                        var stuff = filter[stuff_index];
+                        for(var att_index in stuff) {
+                            var attribute = stuff[att_index];
+                            if(!(attribute.feature_type__code in cleaned_filter)) {
+                                cleaned_filter[attribute.feature_type__code] = []
+                            }
+                            cleaned_filter[attribute.feature_type__code].push(attribute)
                         }
                     }
-                    cleaned_filters.append(cleaned_filter)
+                    for(var type_code in cleaned_filter) {
+                        cleaned_filter[type_code] = this.format(type_code, cleaned_filter[type_code])
+                    }
+                    if(
+                        (filter_index == attrs.filters.length - 1) &&
+                        (filter[0].length == 0)
+                    ) {
+                        attrs.empty_fallback = true;
+                    } else {
+                        attrs.cleaned_filters.push(cleaned_filter)
+                    }
                 }
+            }
+            if( attrs.content ) {
+                attrs.content_url = attrs.content_url;
             }
             return attrs;
         }
--- targetadmin/static/templates/campaignSummary.js
+++ targetadmin/static/templates/campaignSummary.js
@@ -7,51 +7,106 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
 function program1(depth0,data) {
   
+  
+  return "    ";
+  }
+
+function program3(depth0,data) {
+  
+  
+  return "        <div class=\"row\"><div class=\"group\">            Nice job! You've successfully created a Targeted Sharing campaign. Here's your campaign summary—please make sure everything looks right before you hit \"Publish Now.\"        </div></div>    ";
+  }
+
+function program5(depth0,data) {
+  
   var buffer = "", stack1;
-  buffer += "            <div class=\"group\"><div class=\"name\">Audience "
-    + escapeExpression(((stack1 = (data == null || data === false ? data : data.index)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</div>                ";
-  stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  buffer += "            <div class=\"group\">                ";
+  stack1 = helpers['if'].call(depth0, (data == null || data === false ? data : data.index), {hash:{},inverse:self.program(8, program8, data),fn:self.program(6, program6, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "                ";
+  stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(10, program10, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "            </div>        ";
   return buffer;
   }
-function program2(depth0,data) {
+function program6(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "                    ";
-  stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "                ";
+  buffer += "                    <div class=\"name\">Fallback Audience "
+    + escapeExpression(((stack1 = (data == null || data === false ? data : data.index)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div>                ";
   return buffer;
   }
-function program3(depth0,data) {
+
+function program8(depth0,data) {
   
-  var buffer = "", stack1, helper;
-  buffer += "                        <div class=\"value\">";
-  if (helper = helpers.operator) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.operator); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + " ";
-  if (helper = helpers.feature) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.feature); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + " ";
-  if (helper = helpers.value) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.value); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "</div>                    ";
+  
+  return "                    <div class=\"name\">Target Audience</div>                ";
+  }
+
+function program10(depth0,data) {
+  
+  var buffer = "";
+  buffer += "                    <div class=\"value\">"
+    + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
+    + "</div>                ";
+  return buffer;
+  }
+
+function program12(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "            <div class=\"group\"><span class=\"name\">Empty Fallback: </span>            ";
+  stack1 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.empty_fallback), {hash:{},inverse:self.program(15, program15, data),fn:self.program(13, program13, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "            </div>        ";
+  return buffer;
+  }
+function program13(depth0,data) {
+  
+  
+  return "                Yes            ";
+  }
+
+function program15(depth0,data) {
+  
+  
+  return "                No            ";
+  }
+
+function program17(depth0,data) {
+  
+  
+  return "        ";
+  }
+
+function program19(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "            <div class=\"group\"><div><span class=\"name\">Campaign Test URL: </span> "
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.live_snippet_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div>                All your changes have been saved. If you're not ready to publish your campaign now, you can always come back later and pick up where you left off.            </div>        ";
   return buffer;
   }
 
   buffer += "<section class=\"col-sm-10 campaign-summary\"><div class=\"row divided\"><div class=\"col-sm-9\"><h1 class=\"heading\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</h1></div><div class=\"col-sm-3\"><button data-js=\"createCampaignBtn\" type=\"button\" class=\"btn create-campaign-btn\">Create Campaign</button></div></div><div class=\"row\"><div class=\"group\"><div class=\"name\">Campaign Name</div><div class=\"value\">"
+    + "</h1></div><div class=\"col-sm-3\"><button data-js=\"createCampaignBtn\" type=\"button\" class=\"btn create-campaign-btn\">Create Campaign</button></div></div>    ";
+  stack1 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.is_published), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "    <div class=\"row\"><div class=\"group\"><div class=\"name\">Campaign Name</div><div class=\"value\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</div></div>        ";
-  stack1 = helpers.each.call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.filters), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  stack1 = helpers.each.call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.cleaned_filters), {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "        <div class=\"group\"><div class=\"name\">Friend Suggestion Page</div><div class=\"value\">Headline: </div><div class=\"value\">Sub-header: </div><div class=\"value\">Thanks URL: <a href=\""
+  buffer += "        ";
+  stack1 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.cleaned_filters), {hash:{},inverse:self.noop,fn:self.program(12, program12, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "        <div class=\"group\"><div class=\"name\">Friend Suggestion Page</div><div class=\"value\">Headline: "
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.headline)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div><div class=\"value\">Sub-header: "
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.subheader)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div><div class=\"value\">Thanks URL: <a href=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.thanks_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.thanks_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
@@ -75,7 +130,10 @@ function program3(depth0,data) {
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.content_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.content_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</a></div></div></div></section>";
+    + "</a></div></div>        ";
+  stack1 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0.campaign)),stack1 == null || stack1 === false ? stack1 : stack1.is_published), {hash:{},inverse:self.program(19, program19, data),fn:self.program(17, program17, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "    </div></section>";
   return buffer;
   })
 
