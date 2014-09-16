80a36060b0c429c2965b4ca5336b34238952512d
Jesse London <jesselondon@gmail.com>
2014-09-16 01:36:36 -0500

    made admin campaign wizard filter manager layers' headers and underlying data resistant to layer-deletion (and regardless of which layer is deleted)

--- targetadmin/static/js/views/campaignWizard/filters.js
+++ targetadmin/static/js/views/campaignWizard/filters.js
@@ -31,104 +31,99 @@ define(
                 'click *[data-js="prevStep"]': 'goBack'
             },
 
-            initialize: function( options ) {
+            initialize: function (options) {
+                _.extend(this, options);
 
-                _.extend( this, options ); 
+                this.maxCount = 4;
+                this.maxIndex = this.maxCount - 1;
 
                 /* Whenever a filter is added to the collection addAvailableFilter will be invoked. */
                 this.availableFilters = 
                 addFilter.availableFilters = new FilterCollection(
-                    [ ], 
-                    { clientId: this.model.get('clientId') } 
-                ).on( 'add', this.addAvailableFilter, this );
+                    [],
+                    {clientId: this.model.get('clientId')}
+                ).on('add', this.addAvailableFilter, this);
 
                 /* If we are editing a campaign, update the UI to show the current filters
                    enabled. */
-                if( this.campaignModel ) {
-                    this.availableFilters.on( 'sync', this.reflectCampaignState, this );
+                if(this.campaignModel) {
+                    this.availableFilters.on('sync', this.reflectCampaignState, this);
                 }
 
-                this.model.set('filterLayerCount',1);
-                this.model.on('change:name', this.updateName, this );
-                addFilter.on('ageFilterCreated', this.updateAgeFilterUI, this );
+                this.model.set('filterLayerCount', 1);
+                this.model.on('change:name', this.updateName, this);
+                addFilter.on('ageFilterCreated', this.updateAgeFilterUI, this);
 
                 this.render();
-
                 this.addDraggableFunctionality();
-
                 return this;
             },
 
             /* slurp and insert out main template, */
             render: function() {
-
-                if( this.hide ) { this.$el.hide(); }
-
-                this.slurpHtml( {
-                    template: template( this.model.toJSON() ),
-                    insertion: { $el: this.$el.appendTo(this.parentEl) } } );
-
+                if(this.hide) {this.$el.hide();}
+                this.slurpHtml({
+                    template: template(this.model.toJSON()),
+                    insertion: {$el: this.$el.appendTo(this.parentEl)}
+                });
                 this.renderFilters();
-
                 return this;
             },
 
             renderFilters: function() {
-
                 /* (backbone.model) send an ajax request for the filters */
                 this.availableFilters.fetch();
 
-                /* I don't think this if statement is needed */ 
-                if( ! this.model.has('filters') ) {
-
-                    this.slurpHtml( {
-                        template: filterLayerTemplate( {
+                if (!this.model.has('filters')) {
+                    this.slurpHtml({
+                        template: filterLayerTemplate({
                             label: 'Target Audience',
                             removeBtn: false,
                             count: 1
-                        } ),
-                        insertion: { $el: this.templateData.enabledFiltersContainer }
-                    } );
+                        }),
+                        insertion: {$el: this.templateData.enabledFiltersContainer}
+                    });
                 }
             },
 
             /* Adds a client filter into the Available Filter box */
             addAvailableFilter: function(filter) {
-
                 this.templateData.availableFilters.append(
-                    filterTemplate( _.extend( {}, filter.toJSON(), { readable: filter.getReadable() } ) )
+                    filterTemplate(_.extend({}, filter.toJSON(), {readable: filter.getReadable()}))
                 );
             },
 
-            /* Add fallback button is clicked */
-            addFallbackLayer: function(e) {
+            addFallbackLayer: function(event) {
+                /* Add fallback button is clicked */
+                return this._addFallbackLayer($(event.currentTarget));
+            },
 
-                var clickedButton = $(e.currentTarget);
+            _addFallbackLayer: function(clickedButton) {
+                var filters,
+                    filterLayerCount = this.model.get('filterLayerCount');
 
-                if( this.model.get('filterLayerCount') === 4 ) { return; }
+                clickedButton = clickedButton || this.templateData.addFallbackBtn;
+
+                if (filterLayerCount === this.maxCount) {return;}
 
                 clickedButton.fadeOut();
-
-                this.slurpHtml( {
-                    template: filterLayerTemplate( {
-                        label: 'Fallback Audience ' + ( this.model.get('filterLayerCount') ),
+                this.slurpHtml({
+                    template: filterLayerTemplate({
+                        label: 'Fallback Audience ' + filterLayerCount,
                         removeBtn: true,
-                        count: this.model.get('filterLayerCount'),
-                        disableAddBtn: ( this.model.get('filterLayerCount') === 3 ) ? true: false
-                    } ),
-                    insertion: { $el: this.templateData.enabledFiltersContainer }
-                } );
+                        count: filterLayerCount
+                    }),
+                    insertion: {$el: this.templateData.enabledFiltersContainer}
+                });
 
-                //copy filters to new layer
-                clickedButton.closest( this.templateData.filterLayer )
-                                .find('*[data-js="filterContainer"]').children().clone(true).appendTo(
-                    this.templateData.enabledFiltersContainer.children().last().find('*[data-js="filterContainer"]') );
+                /* copy filters to new layer */
+                filters = this.templateData.enabledFiltersContainer.children().last().find('*[data-js="filterContainer"]');
+                clickedButton.closest(this.templateData.filterLayer)
+                    .find('*[data-js="filterContainer"]').children().clone(true).appendTo(filters);
 
-                this.model.set('filterLayerCount', this.model.get('filterLayerCount') + 1 );
-                this.updateFallbackLabels();
-
+                this.model.set('filterLayerCount', filterLayerCount + 1);
+                this.updateFallbackHeaders();
                 this.addDraggableFunctionality();
-
                 return this;
             },
 
@@ -173,92 +168,98 @@ define(
                 source.find('[data-link="' + dataLink + '"]').appendTo(target);
             },
 
-            /* 'X' (remove layer) is clicked */ 
-            removeLayer: function(e) {
+            removeLayer: function(event) {
+                /* 'X' (remove layer) is clicked */ 
+                var self = this,
+                    layerContainer = $(event.currentTarget).closest('[data-js=filterLayer]');
                 
-                var layerContainer = $(e.currentTarget).closest('*[data-js="filterLayer"]'),
-                    self = this;
-                
-                if( layerContainer.find('*:visible[data-js="addFallbackBtn"]').length ) {
-                    layerContainer.prev().find('*[data-js="addFallbackBtn"]').show();
-                }
-
-                //make sure we aren't remove a filter that doesn't exist elsewhere
-                _.each( layerContainer.find('*[data-js="filterContainer"]').children(), function( filter ) {
-                    filter = $(filter);
-                    if( this.$el.find('*[data-filter-id="' + $(filter).attr('data-filter-id') + '"]').length === 1 ) {
-                        filter.appendTo( this.templateData.availableFilters );
+                // make sure we aren't removing a filter that doesn't exist elsewhere:
+                _.each(layerContainer.find('[data-js=filterContainer]').children(), function(filter) {
+                    var $filter = $(filter);
+                    if(this.$el.find('*[data-filter-id="' + $filter.attr('data-filter-id') + '"]').length === 1) {
+                        $filter.appendTo(this.templateData.availableFilters);
                     }
-                }, this );
+                }, this);
 
-
-                layerContainer.fadeOut( 400, function() {
-                    $(this).remove();
-                    self.updateFallbackLabels();
-                } );
-                
-                this.model.set('filterLayerCount', this.model.get('filterLayerCount') - 1 );
+                layerContainer.fadeOut(400, function() {
+                    var $this = $(this),
+                        cache = self.templateData;
+                    $this.remove(); // remove from DOM
+                    cache.filterLayer = cache.filterLayer.not($this); // remove from cache
+                    self.updateFallbackHeaders(); // update interface
+                });
+                this.model.set('filterLayerCount', this.model.get('filterLayerCount') - 1);
             },
 
-            /* after a layer removal make sure the labels still make sense ( numbers ) */
-            updateFallbackLabels: function() {
+            updateFallbackHeaders: function() {
+                /* after a layer addition or removal update labels and buttons */
+                var containers = this.templateData.enabledFiltersContainer.children(),
+                    maxIndex = containers.length - 1;
 
-                _.each( this.templateData.enabledFiltersContainer.children(), function( filterLayerContainer, i ) {
-                    if( i === 0 ) { return; }
-                    $(filterLayerContainer).find('*[data-target="filterLabel"]').text( 'Fallback Audience ' + i);
-                    $(filterLayerContainer).find('*[data-target="filterLayerFormField"]').attr(
-                        'name', 'enabled-filters-' + ( i + 1 ) );
-                }, this );
-                
+                this.templateData.enabledFiltersContainer.find('[data-js=addFallbackBtn]').hide();
+
+                _.each(containers, function(filterLayerContainer, containerIndex) {
+                    var $filterLayerContainer = $(filterLayerContainer);
+
+                    if (containerIndex === maxIndex && containerIndex < this.maxIndex) {
+                        $filterLayerContainer.find('[data-js=addFallbackBtn]').show();
+                    }
+
+                    if (containerIndex === 0) {return;}
+
+                    $filterLayerContainer.find('*[data-target="filterLabel"]').text('Fallback Audience ' + containerIndex);
+                    $filterLayerContainer.find('*[data-target="filterLayerFormField"]').attr(
+                        'name',
+                        'enabled-filters-' + (containerIndex + 1)
+                    );
+                }, this);
             },
 
             /* should be in base class, updates campaign name in header if it changes */
             updateName: function() {
-                this.templateData.campaignName.text( this.model.get('name') );
+                this.templateData.campaignName.text(this.model.get('name'));
             },
 
-            /* Potential bug: 'filterLayerCount' should be updated here */
-            /* Adds current campaign filter state to ui */
             reflectCampaignState: function() {
+                /* Add current campaign filter state to UI */
                 var campaignFilters = this.campaignModel.get('filters');
-                _.each( campaignFilters, function( filterLayer, i ) {
-                    var filter_collection = new FilterCollection(
-                        filterLayer,
-                        { }
-                    )
-                    _.each( filter_collection.models, function( filter ) {
-                        var atts = filter.attributes;
-                        var selector = '*[data-filter-id="set_number=' + atts.feature + '.' + atts.operator + '.' + atts.value + '"]',
-                            fromAvailable = this.templateData.availableFilters.find( selector );
 
-                        if( fromAvailable.length ) { fromAvailable.dblclick(); }
-                        else {
+                _.each(campaignFilters, function(filterLayer, index) {
+                    var nextIndex = index + 1,
+                        filter_collection = new FilterCollection(filterLayer, {});
+
+                    _.each(filter_collection.models, function(filter) {
+                        var atts = filter.attributes,
+                            selector = '*[data-filter-id="set_number=' + atts.feature + '.' + atts.operator + '.' + atts.value + '"]',
+                            fromAvailable = this.templateData.availableFilters.find(selector);
+
+                        if (fromAvailable.length) {
+                            fromAvailable.dblclick();
+                        } else {
                             this.templateData.enabledFiltersContainer
-                              .find( selector ).clone(true).first().appendTo( this.templateData.availableFilters ).dblclick();
+                              .find(selector).clone(true).first().appendTo(this.templateData.availableFilters).dblclick();
                         }
-                    }, this );
+                    }, this);
 
-                    if( campaignFilters.length - 1 != i && campaignFilters[i+1].length != 0 ) {
-                        this.templateData.addFallbackBtn.click();
+                    if(nextIndex < campaignFilters.length && campaignFilters[nextIndex].length !== 0) {
+                        this._addFallbackLayer();
                         this.templateData.filterContainer.last().empty();
                     }
-                }, this );
+                }, this);
             },
 
-            /* shows modal with information regarding how filters work */ 
             showFilterInfo: function() {
-                modal.update( {
+                /* shows modal with information regarding how filters work */ 
+                modal.update({
                     body: filtersInfoTemplate,
                     longContent: true
-                  } );
-
+                });
                 modal.templateData.confirmBtn.hide();
                 modal.templateData.modalContainer.modal();
-                
             },
 
-            /* shows modal with information regarding how empty fallbacks work */ 
             showEmptyFallbackInfo: function() {
+                /* shows modal with information regarding how empty fallbacks work */ 
                 modal.update( {
                     body: filtersInfoTemplate,
                   } );
@@ -297,28 +298,27 @@ define(
                 _.each( [ availableFiltersCount - 2, availableFiltersCount - 1 ], function( i ) {
                     $( this.templateData.availableFilters.children()[ i ] )
                       .hide()
-                      .attr('data-link',min + '-' + max);
+                      .attr('data-link', min + '-' + max);
                 }, this );
 
-                this.templateData.availableFilters.append( combinedAgeFilter );
+                this.templateData.availableFilters.append(combinedAgeFilter);
             },
 
             /* translates filters into input element value for the backend to handle before
                heading to the next step this format was not done by cbaron, it is not good */
-            prepareFormFieldValues: function() {
-                var dummy_filter_neglector = "[data-filter-id!='set_number=..']";
-
-                _.each( _.range( 1, this.model.get('filterLayerCount') + 1 ), function( i ) {
-
-                    var layerContainer = $(this.templateData.filterLayer[i-1]);
-
-                    layerContainer.find('input[name="enabled-filters-' + i + '"]').val(
-                        _.map( layerContainer.find('*[data-js="filterContainer"]').children(dummy_filter_neglector), function( filter ) {
-                            return '"' + $(filter).attr('data-filter-id').split('=')[1] + '"';
-                        }, this )
-                    );
-                }, this );
-
+            prepareFormFieldValues: function () {
+                var dummyFilterNeglector = "[data-filter-id!='set_number=..']";
+                _.each(this.templateData.filterLayer, function(layerContainer, index) {
+                    var layerCount = index + 1,
+                        $layerContainer = $(layerContainer),
+                        $layers = $layerContainer.find('[data-js=filterContainer]').children(dummyFilterNeglector),
+                        layerValues = _.map($layers, function(filter) {
+                            var filterValue = $(filter).attr('data-filter-id').split('=')[1],
+                                escapedValue = filterValue.replace(/"/g, '""');
+                            return '"' + escapedValue + '"';
+                        }, this);
+                    $layerContainer.find('input[name=enabled-filters-' + layerCount + ']').val(layerValues.join(','));
+                }, this);
                 this.trigger('nextStep');
             },
 
--- targetadmin/static/templates/campaignWizard/filterLayer.js
+++ targetadmin/static/templates/campaignWizard/filterLayer.js
@@ -8,12 +8,6 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   
-  return "disabled=\"true\"";
-  }
-
-function program3(depth0,data) {
-  
-  
   return "<span class=\"glyphicon glyphicon-remove\" data-js=\"removeLayerBtn\"></span>";
   }
 
@@ -21,11 +15,8 @@ function program3(depth0,data) {
   if (helper = helpers.label) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.label); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</span><button data-js=\"addFallbackBtn\" type=\"button\" class=\"btn\" ";
-  stack1 = helpers['if'].call(depth0, (depth0 && depth0.disableAddBtn), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += ">            Add Fallback Audience        </button>        ";
-  stack1 = helpers['if'].call(depth0, (depth0 && depth0.removeBtn), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
+    + "</span><button data-js=\"addFallbackBtn\" type=\"button\" class=\"btn\">Add Fallback Audience</button>        ";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.removeBtn), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "    </div><div data-js=\"filterContainer\" class=\"filter-container\" data-type=\"sortable\"></div><input data-target=\"filterLayerFormField\" type=\"hidden\" name=\"enabled-filters-";
   if (helper = helpers.count) { stack1 = helper.call(depth0, {hash:{},data:data}); }
