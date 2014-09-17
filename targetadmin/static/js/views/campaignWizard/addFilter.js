0810e72c044cab18ec28b83bdf5b73541ea3ad17
Tristan Crockett <tristan@edgeflip.com>
2014-09-17 17:26:21 -0500

    Tweak location removal

--- targetadmin/static/js/views/campaignWizard/addFilter.js
+++ targetadmin/static/js/views/campaignWizard/addFilter.js
@@ -221,14 +221,15 @@ define(
 
             /* location removed, remove it from box */
             removeProposedLocation: function(e) {
-                var loc = $(e.currentTarget).parent();
+                var self = this,
+                    loc = $(e.currentTarget).parent();
                 loc.fadeOut( 400, function() {
-                    loc.remove();
-                    if( this.templateData.locationContainer.children().length === 0 ) {
-                        this.templateData.locationContainer.addClass('hide');
-                        this.disableConfirmButton();
+                    $(this).remove();
+                    if( self.templateData.locationContainer.children().length === 0 ) {
+                        self.templateData.locationContainer.addClass('hide');
+                        self.disableConfirmButton();
                     }
-                }.bind(this));
+                });
             },
 
             /* user clicks add filter, so we create a filter model based on the
