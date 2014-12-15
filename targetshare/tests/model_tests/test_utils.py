cc1a7796a565decee3a43377b845ee7b4b8ff54f
Jesse London <jesselondon@gmail.com>
2014-12-15 14:57:48 -0600

    made sure invocation of cached property on ordered enum base class doesn't interfere with property on concrete classes

--- targetshare/tests/model_tests/test_utils.py
+++ targetshare/tests/model_tests/test_utils.py
@@ -106,6 +106,31 @@ def test_enum_aliases():
     yield (tools.assert_less, RedundantStatus.unpublished, RedundantStatus.archived)
 
 
+def test_inheritance_ordinals():
+    """Cached property (behind __member_ordinals__) is inherited but its data is not"""
+    # Subclassing non-empty (i.e. concrete) enum is already disallowed;
+    # but, rather than disallow access to the concrete property on abstract
+    # classes, instead mimic other enum properties, which just return their
+    # empty results, and ensure that the getter works correctly.
+    class SpinOrderedQuarks(OrderedStrEnum):
+
+        UP = 'u'
+        DOWN = 'd'
+        CHARM = 'c'
+        STRANGE = 's'
+        TOP = 't'
+        BOTTOM = 'b'
+
+    # Test base first to challenge cache inheritance
+    base_ordinals = OrderedStrEnum.__member_ordinals__
+    tools.eq_(base_ordinals, {})
+
+    quark_ordinals = SpinOrderedQuarks.__member_ordinals__
+    tools.assert_true(quark_ordinals)
+    tools.assert_is_instance(quark_ordinals, dict)
+    tools.assert_not_equal(quark_ordinals, base_ordinals)
+
+
 # AbstractStatus tests #
 
 class TestAbstractStatus(object):
@@ -121,6 +146,12 @@ class TestAbstractStatus(object):
             __order__ = 'draft, published, archived'
 
         cls.BlogPostStatus = BlogPostStatus
+
+    @tools.raises(NotImplementedError)
+    def test_abstract_choices(self):
+        # choices ain't no magic method, no need to conform to enum properties'
+        # pattern; invocation is considered a mistake.
+        AbstractStatus.choices
 
     def test_choices(self):
         tools.eq_(self.BlogPostStatus.choices, (
