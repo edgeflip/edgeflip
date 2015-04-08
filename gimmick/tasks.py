9e3b16289e48de94885a3917ef0420d8ba47060f
Jesse London <jesse@edgeflip.com>
2015-04-08 06:49:58 -0500

    clean-up

--- gimmick/tasks.py
+++ gimmick/tasks.py
@@ -1,7 +1,11 @@
+import datetime
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.utils import timezone
 
 from gimmick.integration.facebook import client
+from gimmick.models import EngagedUser
 
 
 LOG = get_task_logger(__name__)
@@ -26,3 +30,102 @@ def read_network(token):
     user = client.get_user(token.token)
     friends = client.get_friends(token.token)
     return (user, friends)
+
+
+def update_or_create_user(fbid, data):
+    (engaged_user, created) = EngagedUser.objects.get_or_create(fbid=fbid, defaults=data)
+
+    if not created:
+        for (key, value) in data.iteritems():
+            setattr(engaged_user, key, value)
+        engaged_user.save()
+
+    return engaged_user
+
+
+def make_date(year, month, day):
+    try:
+        return datetime.date(year, month, day)
+    except ValueError:
+        # Feb 29
+        return datetime.date(year, month, day - 1)
+
+
+@shared_task
+def compute_rankings(results):
+    (scored_posts, scored_likes, user_network) = results
+    (post_score, post_stream) = scored_posts
+    (like_score, like_stream) = scored_likes
+    (user, friends) = user_network
+
+    engaged_user = update_or_create_user(user.fbid, {
+        'first_name': user.fname,
+        'last_name': user.lname,
+        'birthday': user.birthday,
+        'city': user.city,
+        'state': user.state,
+        'gender': user.gender,
+        'score': like_score + post_score,
+    })
+
+    data = {
+        'greenest_posts': [
+            {
+                'message': post.message,
+                'post_id': post.post_id,
+                'score': post.score,
+            } for post in post_stream[:3]
+        ],
+        'greenest_likes': [
+            {
+                'name': like.name,
+                'page_id': like.page_id,
+            } for like in like_stream[:3]
+        ],
+        'suggested_pages': [
+            (8492293163, 'Environmental Defense Fund', 'https://www.facebook.com/EnvDefenseFund'),
+            (15687409793, 'World Wildlife Fund', 'https://www.facebook.com/worldwildlifefund'),
+            (8057376739, 'The Nature Conservancy', 'https://www.facebook.com/thenatureconservancy'),
+        ],
+        'city': {
+            'user_city': user.city,
+            'user_state': user.state,
+            'rank': EngagedUser.objects.filter(city=user.city, state=user.state).filter(
+                score__gt=engaged_user.score
+            ).count() + 1,
+        },
+        'friends': {'rank': 1, 'top': []},
+    }
+
+    # Fill in friends rank
+    all_fbids = [friend.fbid for friend in friends] + [user.fbid]
+    friends_using_app = EngagedUser.objects.filter(fbid__in=all_fbids)
+    for (rank, friend) in enumerate(friends_using_app.order_by('-score').iterator(), 1):
+        if friend.fbid == user.fbid:
+            data['friends']['rank'] = rank
+            data['friends']['top'].append({
+                'rank': rank,
+                'fbid': friend.fbid,
+                'first_name': 'You',
+                'last_name': '',
+            })
+        elif rank <= 5:
+            data['friends']['top'].append({
+                'rank': rank,
+                'fbid': friend.fbid,
+                'first_name': friend.first_name,
+                'last_name': friend.last_name,
+            })
+
+    # Add age rank
+    user_age = user.age
+    today = timezone.now().date()
+    start_date = make_date(today.year - user_age - 1, today.month, today.day)
+    end_date = make_date(today.year - user_age, today.month, today.day)
+    user_peers = EngagedUser.objects.filter(birthday__gt=start_date, birthday__lte=end_date)
+    data['age'] = {
+        'user_age': user_age,
+        'rank': user_peers.filter(score__gt=engaged_user.score).count() + 1,
+    }
+
+    return data
--- gimmick/views/engage.py
+++ gimmick/views/engage.py
@@ -1,11 +1,9 @@
-import datetime
 import re
 
 import celery
 from django import http
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
-from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_http_methods
 
@@ -17,7 +15,6 @@ from targetshare.models.relational import FBApp
 from targetshare.tasks.integration.facebook import store_oauth_token
 
 from gimmick import tasks
-from gimmick.models import EngagedUser
 
 
 API_VERSION = make_version('2.3')
@@ -94,7 +91,9 @@ def main(request, appid, signed):
                 api=API_VERSION,
                 app_id=appid,
             ) |
-            celery.group(tasks.score_posts.s(), tasks.score_likes.s(), tasks.read_network.s())
+            celery.group(tasks.score_posts.s(), tasks.score_likes.s(), tasks.read_network.s()) |
+            tasks.compute_rankings.s() # celery won't give us a collected group ID,
+                                       # so post-process in a chord callback
         )
         task = chain.apply_async()
         task_id = request.session[session_key] = task.id
@@ -112,109 +111,12 @@ def data(request, task_id):
         return JsonHttpResponse({'status': 'waiting'}, status=202)
 
     # Check result #
-    result = task.result if task.successful() else None
-    if not result:
+    results = task.result if task.successful() else None
+    if not results:
         return http.HttpResponseServerError()
 
     # TODO: events?
     return JsonHttpResponse({
         'status': 'success',
-        'results': compute_rankings(*result),
+        'results': results,
     })
-
-
-def update_or_create_user(fbid, data):
-    (engaged_user, created) = EngagedUser.objects.get_or_create(fbid=fbid, defaults=data)
-
-    if not created:
-        for (key, value) in data.iteritems():
-            setattr(engaged_user, key, value)
-        engaged_user.save()
-
-    return engaged_user
-
-
-def make_date(year, month, day):
-    try:
-        return datetime.date(year, month, day)
-    except ValueError:
-        # Feb 29
-        return datetime.date(year, month, day - 1)
-
-
-def compute_rankings(scored_posts, scored_likes, user_network):
-    (post_score, post_stream) = scored_posts
-    (like_score, like_stream) = scored_likes
-    (user, friends) = user_network
-
-    engaged_user = update_or_create_user(user.fbid, {
-        'first_name': user.fname,
-        'last_name': user.lname,
-        'birthday': user.birthday,
-        'city': user.city,
-        'state': user.state,
-        'gender': user.gender,
-        'score': like_score + post_score,
-    })
-
-    data = {
-        'greenest_posts': [
-            {
-                'message': post.message,
-                'post_id': post.post_id,
-                'score': post.score,
-            } for post in post_stream[:3]
-        ],
-        'greenest_likes': [
-            {
-                'name': like.name,
-                'page_id': like.page_id,
-            } for like in like_stream[:3]
-        ],
-        'suggested_pages': [
-            (8492293163, 'Environmental Defense Fund', 'https://www.facebook.com/EnvDefenseFund'),
-            (15687409793, 'World Wildlife Fund', 'https://www.facebook.com/worldwildlifefund'),
-            (8057376739, 'The Nature Conservancy', 'https://www.facebook.com/thenatureconservancy'),
-        ],
-        'city': {
-            'user_city': user.city,
-            'user_state': user.state,
-            'rank': EngagedUser.objects.filter(city=user.city, state=user.state).filter(
-                score__gt=engaged_user.score
-            ).count() + 1,
-        },
-        'friends': {'rank': 1, 'top': []},
-    }
-
-    # Fill in friends rank
-    all_fbids = [friend.fbid for friend in friends] + [user.fbid]
-    friends_using_app = EngagedUser.objects.filter(fbid__in=all_fbids)
-    for (rank, friend) in enumerate(friends_using_app.order_by('-score').iterator(), 1):
-        if friend.fbid == user.fbid:
-            data['friends']['rank'] = rank
-            data['friends']['top'].append({
-                'rank': rank,
-                'fbid': friend.fbid,
-                'first_name': 'You',
-                'last_name': '',
-            })
-        elif rank <= 5:
-            data['friends']['top'].append({
-                'rank': rank,
-                'fbid': friend.fbid,
-                'first_name': friend.first_name,
-                'last_name': friend.last_name,
-            })
-
-    # Add age rank
-    user_age = user.age
-    today = timezone.now().date()
-    start_date = make_date(today.year - user_age - 1, today.month, today.day)
-    end_date = make_date(today.year - user_age, today.month, today.day)
-    user_peers = EngagedUser.objects.filter(birthday__gt=start_date, birthday__lte=end_date)
-    data['age'] = {
-        'user_age': user_age,
-        'rank': user_peers.filter(score__gt=engaged_user.score).count() + 1,
-    }
-
-    return data
