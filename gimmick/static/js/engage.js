a15b7ccddbfbbe98b953fcb8c35e9fb10740591e
Tristan Crockett <tristan@edgeflip.com>
2015-04-08 11:29:31 -0500

    Missed var name change

--- gimmick/static/js/engage.js
+++ gimmick/static/js/engage.js
@@ -101,8 +101,7 @@ edgeflip.engage = (function (edgeflip, $) {
             $('#like-list').append('No green likes.');
         } else {
             self.results.greenest_likes.forEach(function (page) {
-                // Available: like.name, like.page_id
-                $('#like-list').append('<li>' + like.name + '</li>');
+                $('#like-list').append('<li>' + page.name + '</li>');
             });
         }
 
