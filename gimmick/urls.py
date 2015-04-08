eca6e24ca128055e58f43d91630df555cc715ea1
Jesse London <jesselondon@gmail.com>
2015-04-08 14:53:39 -0500

    fixed url routing for socially engaged gimmick app in production

--- gimmick/urls.py
+++ gimmick/urls.py
@@ -1,20 +1,17 @@
 from django.conf.urls import patterns, url
 
 
-SHARED_PATTERNS = patterns('gimmick.views',
-    url(r'^engage/$', 'engage.main', name='engage'),
-    url(r'^engage/(?P<task_id>[\w-]+)\.json$', 'engage.data', name='engage-data'),
-    url(r'^engage/intro\.html$', 'engage.intro', name='engage-intro'),
-)
-
-
-urlpatterns = SHARED_PATTERNS + patterns('gimmick.views',
+demopatterns = patterns('gimmick.views',
     url(r'^map/$', 'map.main', name='map'),
     url(r'^map/data\.json$', 'map.data', name='map-data'),
 )
 
 
-canvaspatterns = SHARED_PATTERNS
+urlpatterns = canvaspatterns = patterns('gimmick.views',
+    url(r'^engage/$', 'engage.main', name='engage'),
+    url(r'^engage/(?P<task_id>[\w-]+)\.json$', 'engage.data', name='engage-data'),
+    url(r'^engage/intro\.html$', 'engage.intro', name='engage-intro'),
+)
 
 
 canvaspatterns_root = patterns('',
